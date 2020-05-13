5081a3d2aff56c33d8ceb9f7e17a11514ed7ab0a
richard fontaine <ririfonfon10@gmail.com>
2020-05-13 21:48:43 +0200

    Create image test.lua

---
+++ grandMA3/shared/resource/lib_plugins/work/image test.lua
@@ -0,0 +1,10 @@
+local pluginName     = select(1,...);
+local componentName  = select(2,...); 
+local signalTable    = select(3,...);
+local my_handle      = select(4,...);
+
+local function myFunction(display_handle)
+
+Cmd("list Image 3.")
+
+return myFunction
