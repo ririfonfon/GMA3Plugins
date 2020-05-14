56ac92bf7a0e887e77b47d0c993e36dff3838277
richard fontaine <ririfonfon10@gmail.com>
2020-05-14 14:13:57 +0200

    Update COLOR_1_2.lua

--- grandMA3/shared/resource/lib_plugins/COLOR_1_2/COLOR_1_2.lua
+++ grandMA3/shared/resource/lib_plugins/COLOR_1_2/COLOR_1_2.lua
@@ -3,7 +3,10 @@ COLOR_1_2 v1.0.1.1
 Please note that this will likly break in future version of the console. and to use at your own risk.
 
 Usage
-* Call Plugin "COLOR_1_2"
+* Call Plugin "COLOR_1_2" 
+* Choose Gel color ...
+* You have two Preset Color reference 
+* It remains only to make sequences, chases with these two preset. and you can change the colors on the fly.
 
 Releases:
 * 1.0.0.1 - Inital release
