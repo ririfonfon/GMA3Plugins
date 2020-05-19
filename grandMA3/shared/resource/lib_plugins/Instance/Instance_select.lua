d51ecd7a7b0af7b6d47e3231f195326a33901f21
richard fontaine <ririfonfon10@gmail.com>
2020-05-19 12:10:21 +0200

    Update Instance_select.lua

--- grandMA3/shared/resource/lib_plugins/Instance/Instance_select.lua
+++ grandMA3/shared/resource/lib_plugins/Instance/Instance_select.lua
@@ -13,12 +13,11 @@ Releases:
 * 1.0.0.3 - add backColor & icon
 
 Created by Richard Fontaine "RIRI", April 2020.
---]]
-
-local pluginName    = select(1,...);
-local componentName = select(2,...); 
-local signalTable   = select(3,...);
-local my_handle     = select(4,...);
+--]] 
+local pluginName = select(1, ...);
+local componentName = select(2, ...);
+local signalTable = select(3, ...);
+local my_handle = select(4, ...);
 
 -- ****************************************************************
 -- speed up global functions, by creating local cache 
@@ -26,8 +25,8 @@ local my_handle     = select(4,...);
 -- caring about performance is imperative for plugins with execute function
 -- ****************************************************************
 
-local F=string.format
-local E=Echo
+local F = string.format
+local E = Echo
 
 -- local functions forward declaration
 local CalculateSFID, clamp
@@ -36,7 +35,7 @@ local CalculateSFID, clamp
 -- plugin main entry point 
 -- ****************************************************************
 
-local function Main(display_handle,argument)
+local function Main(display_handle, argument)
 
     -- select all sub fixture 
 
@@ -46,42 +45,45 @@ local function Main(display_handle,argument)
 
     local instance
     local sfIdx = SelectionFirst()
-    
+
     if argument == nil then
         E('Call Plugin Instance selection')
 
         -- Gather information using MessageBox()
 
-        local messageBoxQuestions = {
-            "Instance"
-        }
+        local messageBoxQuestions = {"Instance"}
         local wfInt = "0123456789"
         local messageBoxOptions = {
-            title="Instance Select",
+            title = "Instance Select",
             backColor = "1.7",
-            icon="riri_plugin_O",
-            timeout=nil,
-            timeoutResultCancel=false,
-            timeoutResultID=nil,
-            titleTextColor=nil,
-            messageTextColor=nil,
-            message="Please enter the number of Instance",
-            display= nil,
-            commands={
-                {value=1, name="Done"},
-                {value=0, name="Cancel"}
+            icon = "riri_plugin_O",
+            timeout = nil,
+            timeoutResultCancel = false,
+            timeoutResultID = nil,
+            titleTextColor = nil,
+            messageTextColor = nil,
+            message = "Please enter the number of Instance",
+            display = nil,
+            commands = {
+                {value = 1, name = "Done"}, {value = 0, name = "Cancel"}
             },
-            inputs={
-                {name=messageBoxQuestions[1], value="", maxTextLength = 4, vkPlugin = "TextInputNumOnly", whiteFilter = wfInt}
+            inputs = {
+                {
+                    name = messageBoxQuestions[1],
+                    value = "",
+                    maxTextLength = 4,
+                    vkPlugin = "TextInputNumOnly",
+                    whiteFilter = wfInt
+                }
             }
         }
         local messageBoxResult = MessageBox(messageBoxOptions)
 
         -- get inputs
-        instance = clamp(math.floor(tonumber(messageBoxResult["inputs"][messageBoxQuestions[1]]) or 0), 0, 360)
-        if instance == 0  then
-            return
-        end
+        instance = clamp(math.floor(tonumber(
+                                        messageBoxResult["inputs"][messageBoxQuestions[1]]) or
+                                        0), 0, 360)
+        if instance == 0 then return end
         -- have to filter out the numbers because non text inputs dont respect the black/white Filters.
         local v = "[^0123456789.]"
 
@@ -90,9 +92,8 @@ local function Main(display_handle,argument)
     local selcetionString = ""
     local tt = 0
 
-    while (sfIdx ~= nil)
-    do
-        local result, take =  Checkinstance(sfIdx,instance)
+    while (sfIdx ~= nil) do
+        local result, take = Checkinstance(sfIdx, instance)
         if take == true and tt == 0 then
             selcetionString = selcetionString .. result
             tt = 1
@@ -100,9 +101,9 @@ local function Main(display_handle,argument)
             selcetionString = selcetionString .. " + "
             selcetionString = selcetionString .. result
         end
-        
+
         sfIdx = SelectionNext(sfIdx, true);
-	end
+    end
 
     Cmd("Clear")
     Cmd("Fixture " .. selcetionString)
@@ -113,31 +114,32 @@ end
 -- plugin exit cleanup entry point 
 -- ****************************************************************
 
-local function Cleanup()
-end
+local function Cleanup() end
 
 -- ****************************************************************
 -- plugin execute entry point 
 -- ****************************************************************
 
-local function Execute(Type,...)
-	local func=signalTable[Type]
-	if(func) then
-		func(signalTable,...)
-	else
-		local debug_text=string.format("Execute %s not supported",Type)
-		E(debug_text)
-	end
+local function Execute(Type, ...)
+    local func = signalTable[Type]
+    if (func) then
+        func(signalTable, ...)
+    else
+        local debug_text = string.format("Execute %s not supported", Type)
+        E(debug_text)
+    end
 end
 
-function signalTable:Key(Status,Source,Profile,Token)
-	local debug_text=F("Execute Key (%s) %s UserProfile %d : %s",Status,Source,Profile,Token)
-	E(debug_text)
+function signalTable:Key(Status, Source, Profile, Token)
+    local debug_text = F("Execute Key (%s) %s UserProfile %d : %s", Status,
+                         Source, Profile, Token)
+    E(debug_text)
 end
 
-function signalTable:Fader(Status,Source,Profile,Token,Value)
-	local debug_text=F("Execute Fader (%s) %s UserProfile %d : %s %f",Status,Source,Profile,Token,Value)
-	E(debug_text)
+function signalTable:Fader(Status, Source, Profile, Token, Value)
+    local debug_text = F("Execute Fader (%s) %s UserProfile %d : %s %f", Status,
+                         Source, Profile, Token, Value)
+    E(debug_text)
 end
 
 -- ****************************************************************
@@ -145,7 +147,7 @@ end
 -- ****************************************************************
 
 -- clamp
-function clamp (input, min, max)
+function clamp(input, min, max)
     local i = input
     if i < min then i = min end
     if i > max then i = max end
@@ -154,20 +156,17 @@ end
 
 -- Sub Fixtures (not child fixtures), don't contain an FID. returns the aparent Sub fixture ID based on the root fixture
 
-function Checkinstance(fixture,instance)
+function Checkinstance(fixture, instance)
     local take
     local i = 0
     local inst = {}
-	local result = GetSubfixture(fixture).FID
-	if (result ~= nil) then
-		return result , take
-	end
+    local result = GetSubfixture(fixture).FID
+    if (result ~= nil) then return result, take end
 
-	result = ""
+    result = ""
     local parent
 
-	while (GetSubfixture(fixture):GetClass() == "SubFixture")
-    do
+    while (GetSubfixture(fixture):GetClass() == "SubFixture") do
         i = i + 1
         parent = GetSubfixture(fixture):Parent();
         result = "." .. fixture - parent.SubfixtureIndex .. result
@@ -177,17 +176,17 @@ function Checkinstance(fixture,instance)
 
     result = GetSubfixture(fixture).FID .. result
 
-    if inst[1] == instance  then
+    if inst[1] == instance then
         take = true
     else
         take = false
     end
 
-	return result , take
+    return result, take
 end
 
 -- ****************************************************************
 -- return the entry points of this plugin : 
 -- ****************************************************************
 
-return Main,Cleanup,Execute
+return Main, Cleanup, Execute
