fc7e247103f5b24526571f5b517bf285d50e9b47
richard fontaine <ririfonfon10@gmail.com>
2020-05-23 15:47:01 +0200

    Update testfonction.lua

--- grandMA3/shared/resource/lib_plugins/work/testfonction.lua
+++ grandMA3/shared/resource/lib_plugins/work/testfonction.lua
@@ -1,36 +1,43 @@
-local pluginName    = select(1,...);
-local componentName = select(2,...); 
-local signalTable   = select(3,...);
-local my_handle     = select(4,...);
+local pluginName = select(1, ...);
+local componentName = select(2, ...);
+local signalTable = select(3, ...);
+local my_handle = select(4, ...);
 
-local F=string.format;
-local E=Echo;
-local MacroNb = 17
-local MacroIndex = 13
-local command = ""
-local ColorGel = 1
-local ColorGelNb = 4
-local PColor = 901
+local F = string.format;
+local E = Echo;
+
+local drives = Root().Temp.DriveCollect
+local sep = GetPathSeparator()
+local selectedDrive -- users selected drive
+local options = {} -- popup options
+
 local function myFunction(display_handle)
 
--- command = F("store Macro %d .1 Thru %d . %d",MacroNb ,MacroNb ,MacroIndex)
--- Cmd(command)
-Cmd("store Macro %d .1 Thru %d . %d",MacroNb ,MacroNb ,MacroIndex)
-Cmd('CD Macro ' .. MacroNb)
-Cmd('Set 1 Command "Store Group 999/o" ')
-Cmd('Set 2 Command "Store Preset 25.999/o" ')
-Cmd('Set 3 Command "Group 999 At Preset 25.999')
-Cmd('Set 4 Command "Store Sequence 999 cue 1 /o ')
-Cmd('Set 5 Command "Go Sequence 999 cue 1')
-Cmd('Set 6 Command "Blind On" ')
-Cmd('Set 7 Command "Fixture Thru" ')
-Cmd('Set 8 Command "Down; Down; Down" ')
-Cmd('Set 9 Command "at Gel %d . %d" ',ColorGel ,ColorGelNb)
-Cmd('Set 10 Command "Store preset 4. %d /m" ',PColor)
-Cmd('Set 11 Command "ClearAll;  Preset 25.999; At Preset 25.999" ')
-Cmd('Set 12 Command "Blind Off" ')
-Cmd('Set 13 Command "Off Sequence 999" ')
-Cmd('CD Root')
+    -- grab a list of connected drives
+    for i = 1, drives.count, 1 do
+        table.insert(options, string.format("%s (%s)", drives[i].name,
+                                            drives[i].DriveType))
+    end
+
+    -- present a popup for the user choose (Internal may not work)
+    selectedDrive = PopupInput("Select a disk", display_handle, options)
+
+    -- if the user cancled then exit the plugin
+    if selectedDrive == nil then return end
+
+    local exportPath = GetPathOverrideFor("export",
+                                          drives[selectedDrive + 1].path) .. sep ..
+                           "export.JSON"
+
+    local importPath = GetPathOverrideFor("backups",
+                                          drives[selectedDrive + 1].path)
+
+    E(exportPath)
+    E(importPath)
+    E(drives[selectedDrive + 1].path)
+
+    local dir = DirList(importPath)
+    E(dir)
     
 end
 
