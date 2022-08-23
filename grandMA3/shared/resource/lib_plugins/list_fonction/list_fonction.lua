a2c2f25133d83411174c4c26029575658dd8beb2
richard fontaine <ririfonfon10@gmail.com>
2022-08-23 11:24:51 +0200

    gma3 1.8.1.0

--- grandMA3/shared/resource/lib_plugins/list_fonction/list_fonction.lua
+++ grandMA3/shared/resource/lib_plugins/list_fonction/list_fonction.lua
@@ -1,3 +1,17 @@
+--[[
+list_fonction v1.0.0.2
+Please note that this will likly break in future version of the console. and to use at your own risk.
+
+Usage
+* Call Plugin "list_fonction" 
+
+Releases:
+* 1.0.0.1 - Inital release
+* 1.0.0.2 - gma3 1.8.1.0
+
+Created by Richard Fontaine "RIRI", August 2022.
+--]] --
+
 local pluginName     = select(1,...);
 local componentName  = select(2,...); 
 local signalTable    = select(3,...);
