bcd191c7f81db6dcb08453f78a202c3f2b0477e0
richard fontaine <ririfonfon10@gmail.com>
2023-06-01 02:14:55 +0200

    Update new.lua

--- grandMA3/shared/resource/lib_plugins/ColorLayout/new.lua
+++ grandMA3/shared/resource/lib_plugins/ColorLayout/new.lua
@@ -1,53 +1,40 @@
---	attributeTimings
---	v1.2.1  (MA3 1.9) - 2023-04-15
---	Written by Jeff Farrow @ penlight.ca
---
---  Used to set attribute fade and delay times for the current cue
---  Creates a popup with fields for fade & delay with a swipe to select the attribute
---
---  Thanks to all the users on the LUA Plugins at forum.malighting.com
---  Your tips and insight have been invaluable
---
---	Released under the 'Improve and return' license
---	Please improve and return to me: jeff@penlight.ca
---	Kindly keep some credit in the files if shared
---	Suggestions for improvements also welcome, or just tell me when you use it
-
-
-local function attributeTimings()
-    local attList = {}
-    for k, value in ipairs(DataPool().PresetPools:Children()) do
-        attList[value.name] = value.no
-    end
-
-    local options = {
-        icon = "time",
-        backColor = "Global.PartlySelected",
-        title = "Attribute Cue Timings",
-        message = "Set Attribute Fade / Delay Times for the current cue",
-        commands = { { value = 1, name = "Ok" }, { value = 0, name = "Cancel" } },
-        inputs = { { name = "Fade Time", value = "", whiteFilter = ".0123456789" }, { name = "Delay Time", value = "", whiteFilter = ".0123456789" } },
-        selectors = { { name = "Att", selectedValue = 1, values = attList, type=0 } }
-    }
-
-    local r = MessageBox(options)
-
-    if r.success then
-        if r.result == 0 then
-            Echo('User pressed Cancel')
+local ArgumentFade = {
+    {name = 'ExecTime',   AppFade = AppImp[1].Nr,     UseExTime = 1,     FadeTime = 0},
+    {name = 'Time 0',     AppFade = AppImp[3].Nr,     UseExTime = 0,     FadeTime = 0},
+    {name = 'Time 1',     AppFade = AppImp[5].Nr,     UseExTime = 0,     FadeTime = 1},
+    {name = 'Time 2',     AppFade = AppImp[7].Nr,     UseExTime = 0,     FadeTime = 2},
+    {name = 'Time 4',     AppFade = AppImp[9].Nr,     UseExTime = 0,     FadeTime = 4},
+    {name = 'Time Input', AppFade = AppImp[11].Nr,    UseExTime = 0,     FadeTime = 0}
+}       
+       
+       
+       
+       -- Create Sequences 
+        Cmd('ClearAll /nu')
+        Cmd('Store Sequence ' .. CurrentSeqNr .. ' \'' .. Argument_Fade[k].name .. '\'')
+        -- Add Cmd to Squence
+        Cmd('set seq ' .. CurrentSeqNr .. ' cue \"CueZero\' Property Command=\'Set Layout ' .. TLayNr .. '.' .. LayNr .. ' Appearance=' .. Argument_Fade[k].AppFade .. '\'')
+        if k == 1 then
+            Cmd('set seq ' .. CurrentSeqNr .. ' cue \''.. Argument_Fade[k].name .. '\' Property Command=\'off seq ' .. FirstSeqTime .. ' thru ' .. LastSeqTime .. ' - ' .. CurrentSeqNr .. ' ; set seq ' .. SeqNrStart .. ' thru ' ..SeqNrEnd.. ' UseExecutorTime='.. Argument_Fade[k].UseExTime .. '')
+        elseif k == 6 then        
+        Cmd('set seq ' .. CurrentSeqNr .. ' cue \''.. Argument_Fade[k].name .. '\' Property Command=\'Go Macro ' .. CurrentMacroNr ..  '')
+        else
+            Cmd('set seq ' .. CurrentSeqNr .. ' cue \''.. Argument_Fade[k].name .. '\' Property Command=\'off seq ' .. FirstSeqTime .. ' thru ' .. LastSeqTime .. ' - ' .. CurrentSeqNr .. ' ; set seq ' .. SeqNrStart .. ' thru ' ..SeqNrEnd.. ' UseExecutorTime='.. Argument_Fade[k].UseExTime .. ' ; Set Matricks ' .. MatrickNrStart .. ' Property "FadeFromx" '.. Argument_Fade[k].FadeTime ..' ; Set Matricks ' .. MatrickNrStart .. ' Property "FadeTox" '.. Argument_Fade[k].FadeTime ..'')
         end
-        if r.result == 1 then
-            Echo('User pressed OK')
-            if (r.inputs["Fade Time"] ~= "") then -- test for blank entry
-                Cmd('Set Cue Property "preset%sfade" %s', tostring(r.selectors["Att"]), tostring(r.inputs["Fade Time"]))
-            end
-            if (r.inputs["Delay Time"] ~= "") then -- test for blank entry
-                Cmd('Set Cue Property "preset%sdelay" %s', tostring(r.selectors["Att"]), tostring(r.inputs["Delay Time"]))
-            end
-        end
-    else
-        Echo('User escaped the dialog')
-    end
-end
+        Cmd('set seq ' .. CurrentSeqNr .. ' cue \"OffCue\' Property Command=\'Set Layout ' .. TLayNr .. '.' .. LayNr .. ' Appearance=' .. Argument_Fade[k+1].AppFade .. '\'')
+        Cmd('set seq ' .. CurrentSeqNr .. ' AutoStart=1 AutoStop=1 MasterGoMode=None AutoFix=0 AutoStomp=0')
+        Cmd('set seq ' .. CurrentSeqNr .. ' Tracking=0 WrapAround=1 ReleaseFirstCue=0 RestartMode=1 CommandEnable=1 XFadeReload=0')
+        Cmd('set seq ' .. CurrentSeqNr .. ' OutputFilter="" Priority=0 SoftLTP=1 PlaybackMaster="" XfadeMode=0')
+        Cmd('set seq ' .. CurrentSeqNr .. ' RateMaster="" RateScale=0 SpeedMaster="" SpeedScale=0 SpeedfromRate=0')
+        Cmd('set seq ' .. CurrentSeqNr .. ' InputFilter="" SwapProtect=0 KillProtect=0 IncludeLinkLastGo=1 UseExecutorTime=0 OffwhenOverridden=1 Lock=0')
+        Cmd('set seq ' .. CurrentSeqNr .. ' SequMIB=0 SequMIBMode=1')
+        -- end Sequences
 
-return attributeTimings
+        -- Add Squences to Layout
+        Cmd('Assign Seq ' .. CurrentSeqNr .. ' at Layout ' .. TLayNr)
+        Cmd('Set Layout ' .. TLayNr .. '.' .. LayNr .. ' appearance=' .. Argument_Fade[k+1].AppFade .. ' PosX ' .. LayX .. ' PosY ' .. LayY .. ' PositionW ' .. LayW .. ' PositionH ' .. LayH .. ' VisibilityObjectname=0 VisibilityBar=0 VisibilityIndicatorBar=0')
+
+        LayX = Maf(LayX + LayW + 20)
+        LayNr = Maf(LayNr + 1)
+        CurrentSeqNr = Maf(CurrentSeqNr + 1)
+        -- end Sequences
