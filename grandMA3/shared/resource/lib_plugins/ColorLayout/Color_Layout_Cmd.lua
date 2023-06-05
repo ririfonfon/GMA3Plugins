f592ddfef1fff8a5edd4c3978b46f3a72a6a5067
richard fontaine <ririfonfon10@gmail.com>
2023-06-05 23:11:46 +0200

    Create Color_Layout_Cmd.lua

---
+++ grandMA3/shared/resource/lib_plugins/ColorLayout/Color_Layout_Cmd.lua
@@ -0,0 +1,13 @@
+local F = string.format
+local E = Echo
+local Co = Confirm
+local Maf = math.floor
+
+function Command_Ext_Suite(CurrentSeqNr)
+    Cmd('set seq ' .. CurrentSeqNr .. ' AutoStart=1 AutoStop=1 MasterGoMode=None AutoFix=0 AutoStomp=0')
+    Cmd('set seq ' .. CurrentSeqNr .. ' Tracking=0 WrapAround=1 ReleaseFirstCue=0 RestartMode=1 CommandEnable=1 XFadeReload=0')
+    Cmd('set seq ' .. CurrentSeqNr .. ' OutputFilter="" Priority=0 SoftLTP=1 PlaybackMaster="" XfadeMode=0')
+    Cmd('set seq ' .. CurrentSeqNr .. ' RateMaster="" RateScale=0 SpeedMaster="" SpeedScale=0 SpeedfromRate=0')
+    Cmd('set seq ' .. CurrentSeqNr .. ' InputFilter="" SwapProtect=0 KillProtect=0 IncludeLinkLastGo=1 UseExecutorTime=0 OffwhenOverridden=1 Lock=0')
+    Cmd('set seq ' .. CurrentSeqNr .. ' SequMIB=0 SequMIBMode=1')
+end
