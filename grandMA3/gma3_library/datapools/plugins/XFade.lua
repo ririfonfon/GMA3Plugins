aecd2357933cb1c263d52af51f6c5ba5c41f05a2
richard fontaine <ririfonfon10@gmail.com>
2024-01-06 15:43:54 +0100

    Update XFade.lua

--- grandMA3/gma3_library/datapools/plugins/XFade.lua
+++ grandMA3/gma3_library/datapools/plugins/XFade.lua
@@ -1,14 +1,13 @@
 local function main()
     local root = Root();
-    local TLay = root.ShowData.DataPools.Default.Layouts:Children()
     local MatrickNr = root.ShowData.DataPools.Default.MAtricks:Children()
 	local fx = tonumber(MatrickNr[1]:Get('FadeFromX', Enums.Roles.Display)) or 'None' 
 	local tx = tonumber(MatrickNr[1]:Get('FadeToX', Enums.Roles.Display)) or 'None' 
-	Echo ("FromX = ")
-	Echo (fx)
-    Echo ("ToX = ")
-	Echo (tx)
+	-- Echo ("FromX = ")
+	-- Echo (fx)
+    -- Echo ("ToX = ")
+	-- Echo (tx)
     local text = string.format( '"%.2f > %.2f"', fx , tx )
-    Cmd('Set Layout 2.99 Property "CustomTextText" ' .. text ..' ')
+    Cmd('Set Layout 2.99 Property "CustomTextText" '.. text ..' ')
 end
 return main
