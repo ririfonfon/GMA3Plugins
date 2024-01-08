1f32e8636ef4f6366730f599d8a2e19dfd883657
richard fontaine <ririfonfon10@gmail.com>
2024-01-08 12:31:52 +0100

    Update XFade.lua

--- grandMA3/gma3_library/datapools/plugins/XFade.lua
+++ grandMA3/gma3_library/datapools/plugins/XFade.lua
@@ -1,13 +1,22 @@
 local function main()
     local root = Root();
     local MatrickNr = root.ShowData.DataPools.Default.MAtricks:Children()
-	local fx = tonumber(MatrickNr[1]:Get('FadeFromX', Enums.Roles.Display)) or 'None' 
-	local tx = tonumber(MatrickNr[1]:Get('FadeToX', Enums.Roles.Display)) or 'None' 
-	-- Echo ("FromX = ")
-	-- Echo (fx)
-    -- Echo ("ToX = ")
-	-- Echo (tx)
-    local text = string.format( '"%.2f > %.2f"', fx , tx )
+	local fx = tonumber(MatrickNr[1]:Get('FadeFromX', Enums.Roles.Display)) or 'None'
+	local tx = tonumber(MatrickNr[1]:Get('FadeToX', Enums.Roles.Display)) or 'None'
+    local text
+    if (fx ~= "None" ) then
+        if (tx ~= "None") then
+        text = string.format( '"%.2f > %.2f"', fx , tx )
+        else
+        text = string.format( '"%.2f > %s"', fx , tx )
+        end
+    else
+        if (tx ~= "None") then
+        text = string.format( '"%s > %.2f"', fx , tx )
+        else
+        text = string.format( '"%s > %s"', fx , tx )
+        end
+    end
     Cmd('Set Layout 2.99 Property "CustomTextText" '.. text ..' ')
 end
 return main
