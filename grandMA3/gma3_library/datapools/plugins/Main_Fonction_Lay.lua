118853cd5a2b622a19461ae4178ef388cdbe16a9
richard fontaine <ririfonfon10@gmail.com>
2024-01-09 00:49:57 +0100

    Update Main_Fonction_Lay.lua

--- grandMA3/gma3_library/datapools/plugins/Main_Fonction_Lay.lua
+++ grandMA3/gma3_library/datapools/plugins/Main_Fonction_Lay.lua
@@ -1,25 +1,26 @@
 local function main()
 
     local Select = UserVars()
-    local sel = GetVar(Select, "LC1_Fonction")
-    local axes = GetVar(Select, "LC1_Axes")
-    local layout = GetVar(Select, "LC1_Layout")
-    local element = GetVar(Select, "LC1_Element")
+    local sel = GetVar(Select, "LC_Fonction")
+    local axes = GetVar(Select, "LC_Axes")
+    local layout = GetVar(Select, "LC_Layout")
+    local element = GetVar(Select, "LC_Element")
+    local matrick = tonumber(GetVar(Select, "LC_Matrick"))
 
     if (sel == 1) then
-        Fade(axes,layout,element)
+        Fade(axes,layout,element,matrick)
     elseif (sel == 2) then
-        Delay_From(axes,layout,element)
+        Delay_From(axes,layout,element,matrick)
     elseif (sel == 3) then
-        Delay_To(axes,layout,element)
+        Delay_To(axes,layout,element,matrick)
     elseif (sel == 4) then
-        Phase(axes,layout,element)
+        Phase(axes,layout,element,matrick)
     elseif (sel == 5) then
-        Group(axes,layout,element)
+        Group(axes,layout,element,matrick)
     elseif (sel == 6) then
-        Block(axes,layout,element)
+        Block(axes,layout,element,matrick)
     elseif (sel == 7) then
-        Wings(axes,layout,element)
+        Wings(axes,layout,element,matrick)
     end
 end
 return main
