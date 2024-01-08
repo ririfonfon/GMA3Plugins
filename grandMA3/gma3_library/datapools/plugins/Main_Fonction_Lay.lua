67d1357c2ace21672f2f3ab4342ab6ff375e0c8a
richard fontaine <ririfonfon10@gmail.com>
2024-01-08 23:07:57 +0100

    Update Main_Fonction_Lay.lua

--- grandMA3/gma3_library/datapools/plugins/Main_Fonction_Lay.lua
+++ grandMA3/gma3_library/datapools/plugins/Main_Fonction_Lay.lua
@@ -3,24 +3,23 @@ local function main()
     local Select = UserVars()
     local sel = GetVar(Select, "LC1_Fonction")
     local axes = GetVar(Select, "LC1_Axes")
-    -- Echo(sel)
-    -- Echo(axes)
+    local layout = GetVar(Select, "LC1_Layout")
+    local element = GetVar(Select, "LC1_Element")
+
     if (sel == 1) then
-        Fade(axes)
+        Fade(axes,layout,element)
     elseif (sel == 2) then
-        Delay_From(axes)
+        Delay_From(axes,layout,element)
     elseif (sel == 3) then
-        Delay_From(axes)
+        Delay_To(axes,layout,element)
     elseif (sel == 4) then
-        Delay_To(axes)
+        Phase(axes,layout,element)
     elseif (sel == 5) then
-        Phase(axes)
+        Group(axes,layout,element)
     elseif (sel == 6) then
-        Group(axes)
+        Block(axes,layout,element)
     elseif (sel == 7) then
-        Block(axes)
-    elseif (sel == 8) then
-        Wings(axes)
+        Wings(axes,layout,element)
     end
 end
 return main
