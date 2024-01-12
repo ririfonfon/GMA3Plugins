6c99d7efcb5eb06b6faa48b734a7f447cd6a9579
richard fontaine <ririfonfon10@gmail.com>
2024-01-12 12:34:06 +0100

    Create LC_View.lua

---
+++ grandMA3/gma3_library/datapools/plugins/LC_View.lua
@@ -0,0 +1,39 @@
+--[[
+Releases:
+* 1.1.6.3
+
+Created by Richard Fontaine "RIRI", January 2024.
+--]]
+
+local function main()
+
+    local Select = UserVars()
+    local sel = tonumber(GetVar(Select, "LC_Fonction"))
+    local axes = tonumber(GetVar(Select, "LC_Axes"))
+    local layout = tonumber(GetVar(Select, "LC_Layout"))
+    local element = tonumber(GetVar(Select, "LC_Element"))
+    local matrick = tonumber(GetVar(Select, "LC_Matrick"))
+
+    if (sel == 1) then
+        Fade(axes,layout,element,matrick)
+    elseif (sel == 2) then
+        Delay_From(axes,layout,element,matrick)
+    elseif (sel == 3) then
+        Delay_To(axes,layout,element,matrick)
+    elseif (sel == 4) then
+        Phase(axes,layout,element,matrick)
+    elseif (sel == 5) then
+        Group(axes,layout,element,matrick)
+    elseif (sel == 6) then
+        Block(axes,layout,element,matrick)
+    elseif (sel == 7) then
+        Wings(axes,layout,element,matrick)
+    end
+
+    DelVar(Select, "LC_Fonction")
+    DelVar(Select, "LC_Axes")
+    DelVar(Select, "LC_Layout")
+    DelVar(Select, "LC_Element")
+    DelVar(Select, "LC_Matrick")
+end
+return main
