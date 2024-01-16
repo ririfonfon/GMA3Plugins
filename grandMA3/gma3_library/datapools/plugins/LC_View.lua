3eb902d75a7d9ba8f915bb72391d49406dac6ec2
richard fontaine <ririfonfon10@gmail.com>
2024-01-16 16:22:19 +0100

    Update LC_View.lua

--- grandMA3/gma3_library/datapools/plugins/LC_View.lua
+++ grandMA3/gma3_library/datapools/plugins/LC_View.lua
@@ -8,11 +8,23 @@ Created by Richard Fontaine "RIRI", January 2024.
 local function main()
 
     local Select = UserVars()
+    local axes, layout, element, matrick, seq_call = 0,0,0,0,0
     local sel = tonumber(GetVar(Select, "LC_Fonction"))
-    local axes = tonumber(GetVar(Select, "LC_Axes"))
-    local layout = tonumber(GetVar(Select, "LC_Layout"))
-    local element = tonumber(GetVar(Select, "LC_Element"))
-    local matrick = tonumber(GetVar(Select, "LC_Matrick"))
+    if GetVar(Select, "LC_Axes") then
+        axes = tonumber(GetVar(Select, "LC_Axes"))
+    end
+    if GetVar(Select, "LC_Layout") then
+        layout = tonumber(GetVar(Select, "LC_Layout"))
+    end
+    if GetVar(Select, "LC_Element") then
+        element = tonumber(GetVar(Select, "LC_Element"))
+    end
+    if GetVar(Select, "LC_Matrick") then
+        matrick = tonumber(GetVar(Select, "LC_Matrick"))
+    end
+    if GetVar(Select, "LC_Sequence") then
+        seq_call = GetVar(Select, "LC_Sequence")
+    end
 
     if (sel == 1) then
         Fade(axes,layout,element,matrick)
@@ -28,6 +40,8 @@ local function main()
         Block(axes,layout,element,matrick)
     elseif (sel == 7) then
         Wings(axes,layout,element,matrick)
+    elseif (sel == 8) then
+        Priority(layout,element,seq_call)
     end
 
     DelVar(Select, "LC_Fonction")
@@ -35,5 +49,6 @@ local function main()
     DelVar(Select, "LC_Layout")
     DelVar(Select, "LC_Element")
     DelVar(Select, "LC_Matrick")
+    DelVar(Select, "LC_Sequence")
 end
 return main
