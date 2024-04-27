ec42dbdc3213e74118becb7b1399fe54a312c259
richard fontaine <ririfonfon10@gmail.com>
2024-04-27 18:25:14 +0200

    Create reload_Plug.lua

---
+++ grandMA3/gma3_library/datapools/plugins/reload_Plug.lua
@@ -0,0 +1,12 @@
+--[[
+Releases:
+* 0.0.0.1
+
+Created by Richard Fontaine "RIRI", April 2024.
+--]]
+
+  return function()
+      CmdIndirectWait('ReloadAllPlugins') 
+  end
+
+  -- end reload_Plug.lua
