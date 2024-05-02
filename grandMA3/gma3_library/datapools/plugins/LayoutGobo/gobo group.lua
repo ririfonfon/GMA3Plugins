78acfed5cd482323eb28fe98b7de5a2d83395d64
richard fontaine <ririfonfon10@gmail.com>
2024-05-02 15:15:58 +0200

    Update gobo group.lua

--- grandMA3/gma3_library/datapools/plugins/LayoutGobo/gobo group.lua
+++ grandMA3/gma3_library/datapools/plugins/LayoutGobo/gobo group.lua
@@ -13,7 +13,7 @@ end
 
 
 local function getWheelName(ftype, attribut)
-    Cmd("Clearall")
+    CmdIndirectWait("Clearall")
     CmdIndirectWait("cd root")
     CmdIndirectWait("cd FixtureType '" .. ftype .. "'")
     CmdIndirectWait("cd DMXModes.1.DMXChannels.'*" .. attribut .. "'.1")
@@ -22,10 +22,10 @@ end
 
 local function createAppearances(ft, att, j)
     local gobowheel = getWheelName(ft, att)
-    Cmd("cd ft '" .. ft .. "'.Wheels.'" .. gobowheel .. "'")
+    CmdIndirectWait("cd ft '" .. ft .. "'.Wheels.'" .. gobowheel .. "'")
     local wheel = CmdObj().Destination
     for _, slot in ipairs(wheel:Children()) do
-        Cmd("Store appearance " .. j .. " 'Appearance " .. j .. "'")
+        CmdIndirectWait("Store appearance " .. j .. " 'Appearance " .. j .. "'")
         local objlist = ObjectList("appearance " .. tostring(j))
         j = j + 1
         local obj = objlist[1]
@@ -65,7 +65,7 @@ local function Check_SlotID(att, FixtureID)
         local rang = tonumber(CmdObj().Destination:Count())
         for d = 1, rang, 1 do
             local i = 1
-            Cmd("cd " .. d)                                -- changing destination
+            CmdIndirectWait("cd " .. d)                                -- changing destination
             while i <= #CmdObj().Destination:Children() do -- iterating over the gobos
                 if (tonumber(CmdObj().Destination:Children()[i].WHEELSLOTINDEX) ~= nil) then
                     Slot_ID_[slot_index] = CmdObj().Destination:Children()[i].WHEELSLOTINDEX
@@ -75,7 +75,7 @@ local function Check_SlotID(att, FixtureID)
                 i = i + 1
                 slot_index = slot_index + 1
             end
-            Cmd('Cd ..')
+            CmdIndirectWait('Cd ..')
         end
         return Slot_ID_
     else
@@ -150,7 +150,7 @@ local function CreateSequence(SeqNameF, Preset_Name1F, Preset_Name2F, Preset_Nam
     CmdIndirectWait("delete seq '" .. SeqNameF .. "' /nc") -- delete existing sequence
     local cue = 0
     local length1 = arrayLength(Preset_Name1F)
-    Cmd("Clearall; Group " .. Grp)
+    CmdIndirectWait("Clearall; Group " .. Grp)
     local length2 = 1
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('Gobo2')) ~= nil then
         length2 = arrayLength(Preset_Name2F)
@@ -165,7 +165,7 @@ local function CreateSequence(SeqNameF, Preset_Name1F, Preset_Name2F, Preset_Nam
     end
     Printf("The length 1 is " ..
         length1 .. " and 2 is " .. length2 .. " and 3 is " .. length3 .. " and 4 is " .. length4)
-    Cmd("Clearall")
+    CmdIndirectWait("Clearall")
 
     CmdIndirectWait("store seq '" .. SeqNameF .. "' /nc")
     CmdIndirectWait("store seq '" .. SeqNameF .. "' cue 1 t " .. length1 + length2 + length3 + length4 .. " /nc")
@@ -256,56 +256,52 @@ local function main(display)
     local Index = {}
     local AppIndex = {}
 
-    Cmd("Blind On")
-    Cmd("clearall; Fixture " .. FixtureNum)
+    CmdIndirectWait("Blind On")
+    CmdIndirectWait("clearall; Fixture " .. FixtureNum)
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('Gobo1')) ~= nil then -- check if Fixture has gobo1
         Index[1] = FirstPreset - 1
         Preset_Name[1], PresetIndex, Slot_ID[1] = CreateLabelPresets("Gobo1", Fixture, FirstPreset)
-        -- Slot_ID[1] = Check_SlotID("Gobo1", Fixture)
     end
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('Gobo2')) ~= nil then -- check if Fixture has gobo2
         Index[2] = PresetIndex - 1
         Preset_Name[2], PresetIndex, Slot_ID[2] = CreateLabelPresets("Gobo2", Fixture, PresetIndex)
-        -- Slot_ID[2] = Check_SlotID("Gobo2", Fixture)
     end
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('Gobo3')) ~= nil then -- check if Fixture has gobo3
         Index[3] = PresetIndex - 1
         Preset_Name[3], PresetIndex, Slot_ID[3] = CreateLabelPresets("Gobo3", Fixture, PresetIndex)
-        -- Slot_ID[3] = Check_SlotID("Gobo3", Fixture)
     end
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('EFFECTWHEEL')) ~= nil then -- check if Fixture has gobo3
         Index[4] = PresetIndex - 1
         Preset_Name[4], PresetIndex, Slot_ID[4] = CreateLabelPresets("EFFECTWHEEL", Fixture, PresetIndex)
-        -- Slot_ID[4] = Check_SlotID("EFFECTWHEEL", Fixture)
     end
 
-    Cmd("cd root")
+    CmdIndirectWait("cd root")
 
-    Cmd("clearall; Fixture " .. FixtureNum)
+    CmdIndirectWait("clearall; Fixture " .. FixtureNum)
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('Gobo1')) then -- check if Fixture has gobo1
         AppIndex[1] = AppNr - 1
         AppNr = createAppearances(FixtureType, "Gobo1", AppNr)
     end
-    Cmd("clearall; Fixture " .. FixtureNum)
+    CmdIndirectWait("clearall; Fixture " .. FixtureNum)
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('Gobo2')) then -- check if Fixture has gobo2
         AppIndex[2] = AppNr - 1
         AppNr = createAppearances(FixtureType, "Gobo2", AppNr)
     end
-    Cmd("clearall; Fixture " .. FixtureNum)
+    CmdIndirectWait("clearall; Fixture " .. FixtureNum)
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('Gobo3')) then -- check if Fixture has gobo3
         AppIndex[3] = AppNr - 1
         AppNr = createAppearances(FixtureType, "Gobo3", AppNr)
     end
-    Cmd("clearall; Fixture " .. FixtureNum)
+    CmdIndirectWait("clearall; Fixture " .. FixtureNum)
     if GetUIChannelIndex(SelectionFirst(), GetAttributeIndex('EFFECTWHEEL')) then -- check if Fixture has gobo3
         AppIndex[4] = AppNr - 1
         AppNr = createAppearances(FixtureType, "EFFECTWHEEL", AppNr)
     end
 
-    Cmd("cd root")
+    CmdIndirectWait("cd root")
 
     CreateSequence(SeqName, Preset_Name[1], Preset_Name[2], Preset_Name[3], Preset_Name[4],
         FixtureGroupsNo, Slot_ID[1], Slot_ID[2], Slot_ID[3], Slot_ID[4], Index, AppIndex)
-    Cmd("Blind Off")
+    CmdIndirectWait("Blind Off")
 end
 return main
