09abd414c85a5731524e93c168497ef277826caf
richard fontaine <ririfonfon10@gmail.com>
2024-06-07 12:12:16 +0200

    Update favorie rec.lua

--- grandMA3/gma3_library/datapools/plugins/DEV/favorie rec.lua
+++ grandMA3/gma3_library/datapools/plugins/DEV/favorie rec.lua
@@ -6,20 +6,20 @@ local activeseq = {}
 local macronum = GetVar(UserVars(),'CP2_favouritemacro')         
 macronum = macronum:gsub(' Macro','')         
 macronum = tonumber(macronum)         
-for i=1,#equences do             
+for i=1,#sequences do             
     if sequences[i]:HasActivePlayback() then                 
         table.insert(activeseq,i)             
     end         
 end         
-if #acropool[macronum] == 0 then             
+if #macropool[macronum] == 0 then             
     layoutspool[801]['Macro '..macronum]:Set('visibilityobjectname',true)         
 end         
 Cmd('label macro '..macronum..' '..string.char(34)..'CP2 Favourite'..string.char(34)..' /o')         
-if #acropool[macronum] > 0 then             
+if #macropool[macronum] > 0 then             
     Cmd('delete macro '..macronum..'.1 thru')         
 end         
-Cmd('store macro '..macronum..'.1 thru'..#[DMXUniverse 75.1]tiveseq..' /o')         
-for i=1,#fffffffffffffffftiveseq do             
+Cmd('store macro '..macronum..'.1 thru'..#activeseq..' /o')         
+for i=1,#activeseq do             
     local seqnumber = activeseq[i]             
     macropool[macronum][i]:Set('command','go sequence '..string.char(34)..''..sequences[seqnumber].name..''..string.char(34)..'')         
 end
