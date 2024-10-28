1523fa5ebf941d7e8ea22f37196adbce9f70ecae
richard fontaine <ririfonfon10@gmail.com>
2024-10-29 00:55:04 +0100

    Update test.lua

--- grandMA3/gma3_library/datapools/plugins/DEV/test.lua
+++ grandMA3/gma3_library/datapools/plugins/DEV/test.lua
@@ -1,15 +1,27 @@
 local h_page = nil
 local function main()
-    local targetPage = CurrentExecPage()
-    local pagenumber = targetPage.No
-    local last_pagenumber = h_page
-    Echo('page : ' .. pagenumber)
+    -- local SeqNr = DataPool().Sequences:Children()
+    -- local SeqNr = SelectedSequence():CurrentChild()
+    -- local SeqNr = SelectedSequence()
+    -- local cue = SeqNr:Children()
+    -- local cue = SelectedSequence():CurrentChild()
+    -- local cue = SelectedSequence():Children()
+    -- for k in ipairs(cue) do
+    --     -- Echo('cue : ' .. k .. ' name : ' .. cue[k].name .. ' ' .. cue[k].fade())
+    --     Echo( k .. 'cue : ' .. cue[k].NO .. ' name : ' .. cue[k].name )
+    --     Echo('next : ' )
+    -- end
 
-    if pagenumber ~= last_pagenumber then
-        send_osc('PageNumber', 0, pagenumber)
-        h_page = pagenumber
-        Echo('page : ' .. pagenumber)
+    local infade , outfade
+
+    infade = SelectedSequence().currentcue[1]:Get('cueinfade',Enums.Roles.Display)
+    outfade = SelectedSequence().currentcue[1]:Get('cueoutfade',Enums.Roles.Display)
+    for key, value in ipairs(SelectedSequence():Children()) do
+        if value.No then
+            Echo("Cue # %f is named %s", value.No / 1000, value.Name)
+        end
     end
+    Echo(infade .. ' ' .. outfade)
 end
 
 return main
