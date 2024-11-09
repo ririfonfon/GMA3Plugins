60c0a44abc94763aee26a6c53138f9eb0abca87d
richard fontaine <ririfonfon10@gmail.com>
2024-11-09 19:38:08 +0100

    Update test.lua

--- grandMA3/gma3_library/datapools/plugins/DEV/test.lua
+++ grandMA3/gma3_library/datapools/plugins/DEV/test.lua
@@ -12,16 +12,22 @@ local function main()
     --     Echo('next : ' )
     -- end
 
-    local infade , outfade
+    local infade, outfade, name, number , pre_infade, pre_outfade , post_infade, post_outfade
 
-    infade = SelectedSequence().currentcue[1]:Get('cueinfade',Enums.Roles.Display)
-    outfade = SelectedSequence().currentcue[1]:Get('cueoutfade',Enums.Roles.Display)
+    infade = SelectedSequence().currentcue[1]:Get('cueinfade', Enums.Roles.Display)
+    outfade = SelectedSequence().currentcue[1]:Get('cueoutfade', Enums.Roles.Display)
+    name = SelectedSequence().currentcue[1].name
     for key, value in ipairs(SelectedSequence():Children()) do
         if value.No then
             Echo("Cue # %f is named %s", value.No / 1000, value.Name)
+            if value.Name == name then
+                number = value.No / 1000
+            end
         end
     end
     Echo(infade .. ' ' .. outfade)
+    Echo('name : ' .. name)
+    Echo('number : ' .. number)
 end
 
 return main
