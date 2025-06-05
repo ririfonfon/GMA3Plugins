9ae25cc57da0fadc52a263f5433cd05b4c11bee2
richard fontaine <ririfonfon10@gmail.com>
2025-06-05 19:41:20 +0200

    Update select_seq.lua

--- grandMA3/gma3_library/datapools/plugins/DEV/select_seq.lua
+++ grandMA3/gma3_library/datapools/plugins/DEV/select_seq.lua
@@ -1,18 +1,44 @@
 local function SelectedSeq()
-    local Current_Seq_Name, last_Current_Seq_Name
+    local Current_Seq_Name, last_Current_Seq_Name, Current_Cue_Name, Last_Current_Cue_Name, Current_Part
     local Select = UserVars()
     if not Current_Seq_Name then
         Current_Seq_Name = SelectedSequence().name
+        Current_Cue_Name = SelectedSequence().currentcue.name
         last_Current_Seq_Name = Current_Seq_Name
-        SetVar(Select, "jp_select", Current_Seq_Name)
-        Printf("first Selected Sequence: %s", Current_Seq_Name)
+        Last_Current_Cue_Name = Current_Cue_Name
+        Cmd('Set Layout 12.57 Property CustomTextText=\' ' .. Current_Seq_Name .. ' \'')
+        Cmd('Set Layout 12.58 Property CustomTextText=\' ' .. Current_Cue_Name .. ' \'')
     end
     while true do
         Current_Seq_Name = SelectedSequence().name
+        Current_Cue_Name = SelectedSequence().currentcue.name
+
+
+        local currentCue = GetCurrentCue()
+        local cueParts = currentCue:Children()
+        local targetPartIndex = 1
+        local targetRecipeLineIndex = 1
+        local part = cueParts[targetPartIndex]
+        local recipeLines = part:Children()
+        local recipeLine = recipeLines[targetRecipeLineIndex]
+
+        if recipeLines then
+            for rang, ppart in ipairs(recipeLines) do
+                if ppart ~= nil then
+                    Printf(ppart.values.name)
+                    Printf(ppart.MAtricks.name)
+                end
+            end
+        end
+
         if last_Current_Seq_Name ~= Current_Seq_Name then
             last_Current_Seq_Name = Current_Seq_Name
-            SetVar(Select, "jp_select", Current_Seq_Name)
-            Printf("Set jpselect to : %s", Current_Seq_Name)
+            Cmd('Set Layout 12.57 Property CustomTextText=\' ' .. Current_Seq_Name .. ' \'')
+        end
+
+        if Last_Current_Cue_Name ~= Current_Cue_Name then
+            Last_Current_Cue_Name = Current_Cue_Name
+            Cmd('Set Layout 12.58 Property CustomTextText=\' ' .. Current_Cue_Name .. ' \'')
         end
     end
 end
