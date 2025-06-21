8dda42df617291b93cc42e4240636d304c2d4077
richard fontaine <ririfonfon10@gmail.com>
2025-06-21 16:23:16 +0200

    Create TR_808_Retour_Recipie.lua

---
+++ grandMA3/gma3_library/datapools/plugins/GMA3_ON_OSC/TR_808_GMA3/TR_808_Retour_Recipie.lua
@@ -0,0 +1,79 @@
+--[[
+    Releases:
+    * 0.0.0.1
+
+    Created by Richard Fontaine "RIRI", june 2025.
+
+--]]
+
+local function main()
+    local Select = UserVars()
+    local TR_Sub, TR_Layout, TR_Pool, TR_Fonction, Target
+    if GetVar(Select, "TR_Fonction") then
+        TR_Fonction = tonumber(GetVar(Select, "TR_Fonction"))
+        Printf("TR_Fonction: %i", TR_Fonction)
+    end
+    if GetVar(Select, "TR_Sub") then
+        TR_Sub = GetVar(Select, "TR_Sub")
+        Printf("TR_Sub: %s", TR_Sub)
+    end
+    if GetVar(Select, "TR_Layout") then
+        TR_Layout = tonumber(GetVar(Select, "TR_Layout"))
+        Printf("TR_Layout: %i", TR_Layout)
+    end
+    if GetVar(Select, "TR_Pool") then
+        TR_Pool = tonumber(GetVar(Select, "TR_Pool"))
+        Printf("TR_Pool: %i", TR_Pool)
+    end
+    local SeqNr = ShowData().DataPools[TR_Pool].Sequences:Children()
+    if (TR_Fonction == 1) then
+        for k in ipairs(SeqNr) do
+            if TR_Sub == SeqNr[k].name then
+                if (SeqNr[k][3][1][1].Selection == nil ) then
+                    Target = "Group"
+                else
+                    Target = SeqNr[k][3][1][1].Selection.Name
+                end
+                Printf("Group Name: %s", Target)
+                Cmd('Set DataPool ' ..
+                TR_Pool .. ' Layout ' .. TR_Layout .. ' Property CustomTextText=\' ' .. Target .. ' \'')
+            end
+        end
+        Printf("no_group")
+    elseif (TR_Fonction == 2) then
+        for k in ipairs(SeqNr) do
+            if TR_Sub == SeqNr[k].name then
+                if (SeqNr[k][3][1][1].Selection == nil ) then
+                    Target = "Value"
+                else
+                    Target = SeqNr[k][3][1][1].Values.Name
+                end
+                Printf("Value Name: %s", Target)
+                Cmd('Set DataPool ' ..
+                TR_Pool .. ' Layout ' .. TR_Layout .. ' Property CustomTextText=\' ' .. Target .. ' \'')
+            end
+        end
+    elseif (TR_Fonction == 3) then
+        for k in ipairs(SeqNr) do
+            if TR_Sub == SeqNr[k].name then
+                if (SeqNr[k][3][1][1].Selection == nil ) then
+                    Target = "Value"
+                else
+                    Target = SeqNr[k][3][1][1].MATricks.Name
+                end
+                Printf("MATricks Name: %s", Target)
+                Cmd('Set DataPool ' ..
+                TR_Pool .. ' Layout ' .. TR_Layout .. ' Property CustomTextText=\' ' .. Target .. ' \'')
+            end
+        end
+    end
+
+
+    DelVar(Select, "TR_Sub")
+    DelVar(Select, "TR_Layout")
+    DelVar(Select, "TR_Pool")
+    DelVar(Select, "TR_Fonction")
+end
+
+
+return main
