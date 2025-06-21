a8ac4c776b90fe042b96f253687c5a4f19510f32
richard fontaine <ririfonfon10@gmail.com>
2025-06-21 16:23:02 +0200

    Create ASSIGN_FUNCTION_TO_TAG.lua

---
+++ grandMA3/gma3_library/datapools/plugins/DEV/ASSIGN_FUNCTION_TO_TAG.lua
@@ -0,0 +1,19 @@
+return function()
+    local Select = UserVars()
+    local tag = GetVar(Select, "Tag")
+    local fonction = GetVar(Select, "Function")
+
+    for exec_no = 101, 490 do 
+        local exec= GetExecutor(exec_no) 
+        if exec ~= nil and exec.Object ~= nil then 
+         local object_tag = exec.Object.Tags 
+         Printf(exec .. object_tag) 
+            if object_tag ~= nil and object_tag == '' .. tag ..':0' then 
+                Cmd('Assign ' .. fonction .. 'Executor ' .. exec )
+            end
+        end
+    end
+    DelVar(Select, "Tag")
+    DelVar(Select, "function")
+end
+-- end tag.lua
