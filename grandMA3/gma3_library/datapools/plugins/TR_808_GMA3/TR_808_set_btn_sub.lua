ebf9b120580a39aeda0a02540bb80c388b98df25
richard fontaine <ririfonfon10@gmail.com>
2026-01-13 18:59:38 +0100

    Update TR_808_set_btn_sub.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_808_set_btn_sub.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_808_set_btn_sub.lua
@@ -3,12 +3,12 @@ local function main()
         { name = "Sequence Number", value = "", whiteFilter = "0123456789" },
     }
     local selectors = {
-        { name = "Varia Selector", selectedValue = 3, values = { ["a"] = 1, ["b"] = 2, ["c"] = 3, ["d"] = 4, ["e"] = 5, ["f"] = 6, ["g"] = 7, ["h"] = 8 },                                                                                                   type = 1 },
-        { name = "Sub Selector",   selectedValue = 1, values = { ["Sub 1"] = 1, ["Sub 2"] = 2, ["Sub 3"] = 3, ["Sub 4"] = 4, ["Sub 5"] = 5, ["Sub 6"] = 6, ["Sub 7"] = 7, ["Sub 8"] = 8, ["Sub 9"] = 9, ["Sub 10"] = 10, ["Sub 11"] = 11, ["Sub 12"] = 12 }, type = 1 }
+        { name = "Varia Selector", selectedValue = 6, values = { ["a"] = 1, ["b"] = 2, ["c"] = 3, ["d"] = 4, ["e"] = 5, ["f"] = 6, ["g"] = 7, ["h"] = 8 },                                                   type = 1 },
+        { name = "Sub Selector",   selectedValue = 1, values = { ["1"] = 1, ["2"] = 2, ["3"] = 3, ["4"] = 4, ["5"] = 5, ["6"] = 6, ["7"] = 7, ["8"] = 8, ["9"] = 9, ["10"] = 10, ["11"] = 11, ["12"] = 12 }, type = 1 }
     }
 
     local SeqNum, VariaSel, SeqEnd, subSel
-    local count, c = 1 , 1
+    local count, c = 1, 1
     local varia_min = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' }
     local varia_mag = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' }
     -- open messagebox:
@@ -50,24 +50,33 @@ local function main()
     -- Printf("SeqNum: %d", SeqNum)
     -- Printf("varia_min[VariaSel]: %s", varia_min[VariaSel])
     -- Printf("varia_mag[VariaSel]: %s", varia_mag[VariaSel])
-    for i = SeqNum, SeqEnd, 1 do
-        CmdIndirectWait("Set DataPool 41 Sequence " ..
-            i ..
-            " Cue 1 Property Command=\"Set #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
-            varia_mag[VariaSel]
-            ..
-            "_SUB_#" ..
-            subSel .. "'] Cue " .. count .. " Part 0.1 Property 'Enabled' 1; Set #[DataPool 'TR_808_GMA3'.'Macros'.'" ..
-            varia_min[VariaSel] .. "_Rec_Sub_#" .. subSel .. "']." .. count .. " 'Enabled' 1")
-        CmdIndirectWait("Set DataPool 41 Sequence " ..
-        i ..
-            " Cue 2 Property Command=\"Set #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
-            varia_mag[VariaSel]
-            ..
-            "_SUB_#" ..
-            subSel .. "'] Cue " .. count .. " Part 0.1 Property 'Enabled' 0; Set #[DataPool 'TR_808_GMA3'.'Macros'.'" ..
-            varia_min[VariaSel] .. "_Rec_Sub_#" .. subSel .. "']." .. count .. " 'Enabled' 0")
-        count = count + 1
+    for e = 1, 12, 1 do
+        -- Printf("count: %d", count)
+        for i = SeqNum, SeqEnd, 1 do
+            CmdIndirectWait("Set DataPool 41 Sequence " ..
+                i ..
+                " Cue 1 Property Command=\"Set #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+                varia_mag[VariaSel]
+                ..
+                "_SUB_#" ..
+                subSel ..
+                "'] Cue " .. count .. " Part 0.1 Property 'Enabled' 1; Set #[DataPool 'TR_808_GMA3'.'Macros'.'" ..
+                varia_min[VariaSel] .. "_Rec_Sub_#" .. subSel .. "']." .. count .. " 'Enabled' 1")
+            CmdIndirectWait("Set DataPool 41 Sequence " ..
+                i ..
+                " Cue 2 Property Command=\"Set #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+                varia_mag[VariaSel]
+                ..
+                "_SUB_#" ..
+                subSel ..
+                "'] Cue " .. count .. " Part 0.1 Property 'Enabled' 0; Set #[DataPool 'TR_808_GMA3'.'Macros'.'" ..
+                varia_min[VariaSel] .. "_Rec_Sub_#" .. subSel .. "']." .. count .. " 'Enabled' 0")
+            count = count + 1
+        end
+        SeqNum = SeqEnd + 2
+        SeqEnd = SeqNum + 15
+        count = 1
+        subSel = subSel + 1
     end
 end
 
