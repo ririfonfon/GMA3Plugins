f4f3af486fab7a6675b1070cb05138c17f335543
richard fontaine <ririfonfon10@gmail.com>
2026-01-20 17:47:20 +0100

    Create TR_CONST_macro_tag_ooftag_sub.lua

---
+++ grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_CONST_macro_tag_ooftag_sub.lua
@@ -0,0 +1,93 @@
+local function main()
+    local inputs = {
+        { name = "macro Number", value = "", whiteFilter = "0123456789" },
+    }
+    local selectors = {
+        { name = "Varia Selector", selectedValue = 1, values = { ["a"] = 1, ["b"] = 2, ["c"] = 3, ["d"] = 4, ["e"] = 5, ["f"] = 6, ["g"] = 7, ["h"] = 8 },                                                   type = 1 },
+        { name = "Sub Selector",   selectedValue = 1, values = { ["1"] = 1, ["2"] = 2, ["3"] = 3, ["4"] = 4, ["5"] = 5, ["6"] = 6, ["7"] = 7, ["8"] = 8, ["9"] = 9, ["10"] = 10, ["11"] = 11, ["12"] = 12 }, type = 1 }
+    }
+
+    local MacroNum, VariaSel, MacroEnd, subSel, lay_object
+    local varia_min = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' }
+    local varia_mag = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' }
+    -- open messagebox:
+    local resultTable =
+        MessageBox(
+            {
+                title = "create macro for TR-808",
+                message = "Please enter the macro number.",
+                message_align_h = Enums.AlignmentH.Left,
+                message_align_v = Enums.AlignmentV.Top,
+                commands = { { value = 1, name = "Ok" }, { value = 0, name = "Cancel" } },
+                inputs = inputs,
+                selectors = selectors,
+                backColor = "Global.Default",
+                icon = "logo_small",
+                titleTextColor = "Global.AlertText",
+                messageTextColor = "Global.Text",
+                autoCloseOnInput = true
+            }
+        )
+
+    -- print results:
+
+    for k, v in pairs(resultTable.inputs) do
+        Printf("Input '%s' = '%s'", k, v)
+        MacroNum = tonumber(v)
+        MacroEnd = MacroNum + 11
+    end
+    for k, v in pairs(resultTable.selectors) do
+        Printf("Selector '%s' = '%d'", k, v)
+        if k == "Varia Selector" then
+            VariaSel = v
+        elseif k == "Sub Selector" then
+            subSel = v
+        end
+    end
+
+    CmdIndirectWait('Delete DataPool 41 Macro ' .. MacroNum .. ' Thru ' .. MacroEnd .. '')
+    for i = MacroNum, MacroEnd, 1 do
+        CmdIndirect('Store DataPool 41 Macro ' .. i .. ' \'' .. varia_min[VariaSel] .. '_Tag_Sub_#' .. subSel)
+        CmdIndirectWait('ChangeDestination DataPool 41 Macro ' .. i .. '')
+        CmdIndirectWait('Insert')
+        CmdIndirectWait("set 1 Command=\"Assign #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+            varia_mag[VariaSel] .. "_SUB_#" ..
+            subSel .. "'] at #[Tag 'Selected_" .. varia_mag[VariaSel] .. "']")
+        CmdIndirectWait('Insert')
+        CmdIndirectWait("set 2 Command=\"Assign #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+            varia_min[VariaSel] .. "_btn_mute_#" ..
+            subSel .. "'] at #[Tag 'Select_Mute_" .. varia_mag[VariaSel] .. "']")
+        CmdIndirectWait('Insert')
+        CmdIndirectWait("set 3 Command=\"Assign #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+            varia_min[VariaSel] .. "_btn_solo_#" ..
+            subSel .. "'] at #[Tag 'Select_Solo_" .. varia_mag[VariaSel] .. "']")
+        subSel = subSel + 1
+    end
+
+    MacroNum = MacroEnd + 6
+    MacroEnd = MacroNum + 11
+    subSel = 1
+
+    CmdIndirectWait('Delete DataPool 41 Macro ' .. MacroNum .. ' Thru ' .. MacroEnd .. '')
+    for i = MacroNum, MacroEnd, 1 do
+        CmdIndirect('Store DataPool 41 Macro ' .. i .. ' \'' .. varia_min[VariaSel] .. '_Off_Tag_Sub_#' .. subSel)
+        CmdIndirectWait('ChangeDestination DataPool 41 Macro ' .. i .. '')
+        CmdIndirectWait('Insert')
+        CmdIndirectWait("set 1 Command=\"Assign Off #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+            varia_mag[VariaSel] .. "_SUB_#" ..
+            subSel .. "'] at #[Tag 'Selected_" .. varia_mag[VariaSel] .. "']")
+        CmdIndirectWait('Insert')
+        CmdIndirectWait("set 2 Command=\"Assign Off #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+            varia_min[VariaSel] .. "_btn_mute_#" ..
+            subSel .. "'] at #[Tag 'Select_Mute_" .. varia_mag[VariaSel] .. "']")
+        CmdIndirectWait('Insert')
+        CmdIndirectWait("set 3 Command=\"Assign Off #[DataPool 'TR_808_GMA3'.'Sequences'.'" ..
+            varia_min[VariaSel] .. "_btn_solo_#" ..
+            subSel .. "'] at #[Tag 'Select_Solo_" .. varia_mag[VariaSel] .. "']")
+        subSel = subSel + 1
+    end
+
+    CmdIndirectWait('ChangeDestination Root')
+end
+
+return main
