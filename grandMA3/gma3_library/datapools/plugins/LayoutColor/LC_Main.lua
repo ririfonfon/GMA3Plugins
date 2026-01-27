8d10e4dbb368bddb541025cf1856f400a58211e0
richard fontaine <ririfonfon10@gmail.com>
2026-01-27 22:32:42 +0100

    Update LC_Main.lua

--- grandMA3/gma3_library/datapools/plugins/LayoutColor/LC_Main.lua
+++ grandMA3/gma3_library/datapools/plugins/LayoutColor/LC_Main.lua
@@ -887,7 +887,7 @@ local function Main(displayHandle)
             OkButton.BackColor = colorBackground
         end
         Obj.Delete(screenOverlay, Obj.Index(baseInput))
-        Construct_Layout(displayHandle, TLay, SeqNrStart, MacroNrStart, MatrickNrStart, MatrickNr, TLayNr, AppNr,
+        Construct_Layout(TLay, SeqNrStart, MacroNrStart, MatrickNrStart, MatrickNr, TLayNr, AppNr,
             All_5_Current, All_5_NrStart, ColPath, SelectedGelNr, SelectedGrp, SelectedGrpNo, TLayNrRef, NaLay, MaxColLgn,
             Favourite_Nr, pool_construct)
     end
