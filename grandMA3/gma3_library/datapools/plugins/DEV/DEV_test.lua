721e822aaad7a9881bf3bf5a309c0619e7d3df31
richard fontaine <ririfonfon10@gmail.com>
2026-01-27 22:30:29 +0100

    Update DEV_test.lua

--- grandMA3/gma3_library/datapools/plugins/DEV/DEV_test.lua
+++ grandMA3/gma3_library/datapools/plugins/DEV/DEV_test.lua
@@ -1,61 +1,326 @@
 local function main()
     local pool_construct = 42
+    local TLay = Root().ShowData.DataPools[pool_construct].Layouts:Children()
     local Preset25 = Root().ShowData.DataPools[pool_construct].PresetPools:Children()
     local Preset25Object = Root().ShowData.DataPools[pool_construct].PresetPools[25]
     local ColPath = Root().ShowData.GelPools
     local ColGels = ColPath:Children()
     local SelectedGelNr = 1
-    local TCol = ColPath:Children()[SelectedGelNr] -- ma gel
     local All_5_Current = 1
-    local prefix = "test "
     local value
+    local SelectedGrp = { 'PIXEL RGB', 'SPOT CONTRE', 'MEGA POINTE SOL', 'RIVALE UP', 'BMFL', 'FLOOR PLATE STRIKE',
+        'UP PLATE STRIKE' }
+    local SelectedGrpNo = { '1', '2', '3', '4', '5', '6', '7' }
+    local SeqNrStart = 2
+    local MacroNrStart = 1
+    local MatrickNr = Root().ShowData.DataPools[pool_construct].Matricks:Children()
+    local MatrickNrStart = 1
+    local NaLay = "Colors"
+    local TLayNr = 2
+    local App = Root().ShowData.Appearances:Children()
+    local AppNr
+    for k in ipairs(App) do
+        AppNr = App[k].NO + 1
+    end
+    if AppNr == nil then
+        AppNr = 1
+    end
+
+
+
+    local Macro_Pool = Root().ShowData.DataPools[pool_construct].Macros
+    local Data_Pool_Nr = Root().ShowData.DataPools[pool_construct].No
+    local All_5_NrEnd
+    local Img = Root().ShowData.MediaPools.Symbols:Children()
+    local ImgNr
+    for k in pairs(Img) do
+        ImgNr = math.floor(Img[k].NO)
+    end
+    if ImgNr == nil then
+        ImgNr = 1
+    end
+    local ImgImp = {
+        { Name = "\"on\"",            FileName = "\"on.png\"",            Filepath = "" },
+        { Name = "\"off\"",           FileName = "\"off.png\"",           Filepath = "" },
+        { Name = "\"exec_time_on\"",  FileName = "\"exec_time_on.png\"",  Filepath = "" },
+        { Name = "\"exec_time_off\"", FileName = "\"exec_time_off.png\"", Filepath = "" },
+        { Name = "\"calcul_on\"",     FileName = "\"calcul_on.png\"",     Filepath = "" },
+        { Name = "\"calcul_off\"",    FileName = "\"calcul_off.png\"",    Filepath = "" },
+        { Name = "\"x_on\"",          FileName = "\"x_on.png\"",          Filepath = "" },
+        { Name = "\"x_off\"",         FileName = "\"x_off.png\"",         Filepath = "" },
+        { Name = "\"y_on\"",          FileName = "\"y_on.png\"",          Filepath = "" },
+        { Name = "\"y_off\"",         FileName = "\"y_off.png\"",         Filepath = "" },
+        { Name = "\"z_on\"",          FileName = "\"z_on.png\"",          Filepath = "" },
+        { Name = "\"z_off\"",         FileName = "\"z_off.png\"",         Filepath = "" }
+    }
+    local NrAppear
+    local NrNeed
+    local StAppExecOn = '\"Showdata.MediaPools.Symbols.exec_time_on\"'
+    local StAppExecOff = '\"Showdata.MediaPools.Symbols.exec_time_off\"'
+    local StAppCalculOn = '\"Showdata.MediaPools.Symbols.calcul_on\"'
+    local StAppCalculOff = '\"Showdata.MediaPools.Symbols.calcul_off\"'
+    local StApp0On = '\"Showdata.MediaPools.Symbols.[number_0_black_png]\"'
+    local StApp0Off = '\"Showdata.MediaPools.Symbols.[number_0_white_png]\"'
+    local StApp1On = '\"Showdata.MediaPools.Symbols.[number_1_black_png]\"'
+    local StApp1Off = '\"Showdata.MediaPools.Symbols.[number_1_white_png]\"'
+    local StApp2On = '\"Showdata.MediaPools.Symbols.[number_2_black_png]\"'
+    local StApp2Off = '\"Showdata.MediaPools.Symbols.[number_2_white_png]\"'
+    local StApp3On = '\"Showdata.MediaPools.Symbols.[number_3_black_png]\"'
+    local StApp3Off = '\"Showdata.MediaPools.Symbols.[number_3_white_png]\"'
+    local StApp4On = '\"Showdata.MediaPools.Symbols.[number_4_black_png]\"'
+    local StApp4Off = '\"Showdata.MediaPools.Symbols.[number_4_white_png]\"'
+    local StAppSkullOn = '\"Showdata.MediaPools.Symbols.[skull_black_png]\"'
+    local StAppSkullOff = '\"Showdata.MediaPools.Symbols.[skull_white_png]\"'
+    local StAppTricksOn = '\"Showdata.MediaPools.Symbols.[arrow_right_black_png]\"'
+    local StAppTricksOff = '\"Showdata.MediaPools.Symbols.[home_white_png]\"'
+    local StAppMacroTricks = '\"Showdata.MediaPools.Symbols.[gear_white_png]\"'
+    local StAppXOn = '\"Showdata.MediaPools.Symbols.x_on\"'
+    local StAppXOff = '\"Showdata.MediaPools.Symbols.x_off\"'
+    local StAppYOn = '\"Showdata.MediaPools.Symbols.y_on\"'
+    local StAppYOff = '\"Showdata.MediaPools.Symbols.y_off\"'
+    local StAppZOn = '\"Showdata.MediaPools.Symbols.z_on\"'
+    local StAppZOff = '\"Showdata.MediaPools.Symbols.z_off\"'
+    local surfix = { 'x', 'y', 'z' }
+    local FadeRef = ' color=\'0,0.8,0,1\''
+    local DelayRef = ' color=\'0.8,0.8,0,1\''
+    local DelayToRef = ' color=\'0.8,0.3,0,1\''
+    local XgrpRef = ' color=\'0,0.8,0.8,1\''
+    local XblockRef = ' color=\'0.8,0,0.8,1\''
+    local XwingsRef = ' color=\'0.8,0,0.3,1\''
+    local PhaseRef = ' color=\'0.3,0,0.8,1\''
+    local SkullRef = ' color=\'0.6,0,0,1\''
+    local NoRef = '1,1,1,1'
+
+    local AppTricks = {
+        { Name = '\'tricks_on\'',       StApp = StAppTricksOn,    Nr = '', RGBref = NoRef },
+        { Name = '\'tricks_off\'',      StApp = StAppTricksOff,   Nr = '', RGBref = NoRef },
+        { Name = '\'macrotricks_off\'', StApp = StAppMacroTricks, Nr = '', RGBref = NoRef }
+    }
+
+    local AppImp = {
+        { Name = '\'exectime_on\'',       StApp = StAppExecOn,    Nr = '', RGBref = FadeRef },
+        { Name = '\'exectime_off\'',      StApp = StAppExecOff,   Nr = '', RGBref = FadeRef },
+        { Name = '\'fade0_on\'',          StApp = StApp0On,       Nr = '', RGBref = FadeRef },
+        { Name = '\'fade0_off\'',         StApp = StApp0Off,      Nr = '', RGBref = FadeRef },
+        { Name = '\'fade1_on\'',          StApp = StApp1On,       Nr = '', RGBref = FadeRef },
+        { Name = '\'fade1_off\'',         StApp = StApp1Off,      Nr = '', RGBref = FadeRef },
+        { Name = '\'fade2_on\'',          StApp = StApp2On,       Nr = '', RGBref = FadeRef },
+        { Name = '\'fade2_off\'',         StApp = StApp2Off,      Nr = '', RGBref = FadeRef },
+        { Name = '\'fade4_on\'',          StApp = StApp4On,       Nr = '', RGBref = FadeRef },
+        { Name = '\'fade4_off\'',         StApp = StApp4Off,      Nr = '', RGBref = FadeRef },
+        { Name = '\'input_fade_on\'',     StApp = StAppCalculOn,  Nr = '', RGBref = FadeRef },
+        { Name = '\'input_fade_off\'',    StApp = StAppCalculOff, Nr = '', RGBref = FadeRef },
+        { Name = '\'delay0_on\'',         StApp = StApp0On,       Nr = '', RGBref = DelayRef },
+        { Name = '\'delay0_off\'',        StApp = StApp0Off,      Nr = '', RGBref = DelayRef },
+        { Name = '\'delay1_on\'',         StApp = StApp1On,       Nr = '', RGBref = DelayRef },
+        { Name = '\'delay1_off\'',        StApp = StApp1Off,      Nr = '', RGBref = DelayRef },
+        { Name = '\'delay2_on\'',         StApp = StApp2On,       Nr = '', RGBref = DelayRef },
+        { Name = '\'delay2_off\'',        StApp = StApp2Off,      Nr = '', RGBref = DelayRef },
+        { Name = '\'delay4_on\'',         StApp = StApp4On,       Nr = '', RGBref = DelayRef },
+        { Name = '\'delay4_off\'',        StApp = StApp4Off,      Nr = '', RGBref = DelayRef },
+        { Name = '\'input_delay_on\'',    StApp = StAppCalculOn,  Nr = '', RGBref = DelayRef },
+        { Name = '\'input_delay_off\'',   StApp = StAppCalculOff, Nr = '', RGBref = DelayRef },
+        { Name = '\'delayto0_on\'',       StApp = StApp0On,       Nr = '', RGBref = DelayToRef },
+        { Name = '\'delayto0_off\'',      StApp = StApp0Off,      Nr = '', RGBref = DelayToRef },
+        { Name = '\'delayto1_on\'',       StApp = StApp1On,       Nr = '', RGBref = DelayToRef },
+        { Name = '\'delayto1_off\'',      StApp = StApp1Off,      Nr = '', RGBref = DelayToRef },
+        { Name = '\'delayto2_on\'',       StApp = StApp2On,       Nr = '', RGBref = DelayToRef },
+        { Name = '\'delayto2_off\'',      StApp = StApp2Off,      Nr = '', RGBref = DelayToRef },
+        { Name = '\'delayto4_on\'',       StApp = StApp4On,       Nr = '', RGBref = DelayToRef },
+        { Name = '\'delayto4_off\'',      StApp = StApp4Off,      Nr = '', RGBref = DelayToRef },
+        { Name = '\'input_delayto_on\'',  StApp = StAppCalculOn,  Nr = '', RGBref = DelayToRef },
+        { Name = '\'input_delayto_off\'', StApp = StAppCalculOff, Nr = '', RGBref = DelayToRef },
+        { Name = '\'xgroup0_on\'',        StApp = StApp0On,       Nr = '', RGBref = XgrpRef },
+        { Name = '\'xgroup0_off\'',       StApp = StApp0Off,      Nr = '', RGBref = XgrpRef },
+        { Name = '\'xgroup2_on\'',        StApp = StApp2On,       Nr = '', RGBref = XgrpRef },
+        { Name = '\'xgroup2_off\'',       StApp = StApp2Off,      Nr = '', RGBref = XgrpRef },
+        { Name = '\'xgroup3_on\'',        StApp = StApp3On,       Nr = '', RGBref = XgrpRef },
+        { Name = '\'xgroup3_off\'',       StApp = StApp3Off,      Nr = '', RGBref = XgrpRef },
+        { Name = '\'xgroup4_on\'',        StApp = StApp4On,       Nr = '', RGBref = XgrpRef },
+        { Name = '\'xgroup4_off\'',       StApp = StApp4Off,      Nr = '', RGBref = XgrpRef },
+        { Name = '\'input_xgroup_on\'',   StApp = StAppCalculOn,  Nr = '', RGBref = XgrpRef },
+        { Name = '\'input_xgroup_off\'',  StApp = StAppCalculOff, Nr = '', RGBref = XgrpRef },
+        { Name = '\'xblock0_on\'',        StApp = StApp0On,       Nr = '', RGBref = XblockRef },
+        { Name = '\'xblock0_off\'',       StApp = StApp0Off,      Nr = '', RGBref = XblockRef },
+        { Name = '\'xblock2_on\'',        StApp = StApp2On,       Nr = '', RGBref = XblockRef },
+        { Name = '\'xblock2_off\'',       StApp = StApp2Off,      Nr = '', RGBref = XblockRef },
+        { Name = '\'xblock3_on\'',        StApp = StApp3On,       Nr = '', RGBref = XblockRef },
+        { Name = '\'xblock3_off\'',       StApp = StApp3Off,      Nr = '', RGBref = XblockRef },
+        { Name = '\'xblock4_on\'',        StApp = StApp4On,       Nr = '', RGBref = XblockRef },
+        { Name = '\'xblock4_off\'',       StApp = StApp4Off,      Nr = '', RGBref = XblockRef },
+        { Name = '\'input_xblock_on\'',   StApp = StAppCalculOn,  Nr = '', RGBref = XblockRef },
+        { Name = '\'input_xblock_off\'',  StApp = StAppCalculOff, Nr = '', RGBref = XblockRef },
+        { Name = '\'xwings0_on\'',        StApp = StApp0On,       Nr = '', RGBref = XwingsRef },
+        { Name = '\'xwings0_off\'',       StApp = StApp0Off,      Nr = '', RGBref = XwingsRef },
+        { Name = '\'xwings2_on\'',        StApp = StApp2On,       Nr = '', RGBref = XwingsRef },
+        { Name = '\'xwings2_off\'',       StApp = StApp2Off,      Nr = '', RGBref = XwingsRef },
+        { Name = '\'xwings3_on\'',        StApp = StApp3On,       Nr = '', RGBref = XwingsRef },
+        { Name = '\'xwings3_off\'',       StApp = StApp3Off,      Nr = '', RGBref = XwingsRef },
+        { Name = '\'xwings4_on\'',        StApp = StApp4On,       Nr = '', RGBref = XwingsRef },
+        { Name = '\'xwings4_off\'',       StApp = StApp4Off,      Nr = '', RGBref = XwingsRef },
+        { Name = '\'input_xwings_on\'',   StApp = StAppCalculOn,  Nr = '', RGBref = XwingsRef },
+        { Name = '\'input_xwings_off\'',  StApp = StAppCalculOff, Nr = '', RGBref = XwingsRef },
+        { Name = '\'input_phase_on\'',    StApp = StAppCalculOn,  Nr = '', RGBref = PhaseRef },
+        { Name = '\'input_phase_off\'',   StApp = StAppCalculOff, Nr = '', RGBref = PhaseRef },
+        { Name = '\'skull_on\'',          StApp = StAppSkullOn,   Nr = '', RGBref = SkullRef },
+        { Name = '\'skull_off\'',         StApp = StAppSkullOff,  Nr = '', RGBref = SkullRef },
+        { Name = '\'x_on\'',              StApp = StAppXOn,       Nr = '', RGBref = NoRef },
+        { Name = '\'x_off\'',             StApp = StAppXOff,      Nr = '', RGBref = NoRef },
+        { Name = '\'y_on\'',              StApp = StAppYOn,       Nr = '', RGBref = NoRef },
+        { Name = '\'y_off\'',             StApp = StAppYOff,      Nr = '', RGBref = NoRef },
+        { Name = '\'z_on\'',              StApp = StAppZOn,       Nr = '', RGBref = NoRef },
+        { Name = '\'z_off\'',             StApp = StAppZOff,      Nr = '', RGBref = NoRef }
+    }
+
+    local Argument_Fade = {
+        { name = 'ExecTime',   UseExTime = 1, Time = 0 },
+        { name = 'Time 0',     UseExTime = 0, Time = 0 },
+        { name = 'Time 1',     UseExTime = 0, Time = 1 },
+        { name = 'Time 2',     UseExTime = 0, Time = 2 },
+        { name = 'Time 4',     UseExTime = 0, Time = 4 },
+        { name = 'Time Input', UseExTime = 0, Time = 0 }
+    }
+
+    local Argument_Delay = {
+        { name = 'Delay From 0',     Time = 0 },
+        { name = 'Delay From 1',     Time = 1 },
+        { name = 'Delay From 2',     Time = 2 },
+        { name = 'Delay From 4',     Time = 4 },
+        { name = 'Delay From Input', Time = 0 },
+    }
+
+    local Argument_DelayTo = {
+        { name = 'Delay To 0',     Time = 0 },
+        { name = 'Delay To 1',     Time = 1 },
+        { name = 'Delay To 2',     Time = 2 },
+        { name = 'Delay To 4',     Time = 4 },
+        { name = 'Delay To Input', Time = 0 },
+    }
+
+    local Argument_Xgrp = {
+        { name = 'XGroup To 0',     Time = 0 },
+        { name = 'XGroup To 2',     Time = 2 },
+        { name = 'XGroup To 3',     Time = 3 },
+        { name = 'XGroup To 4',     Time = 4 },
+        { name = 'XGroup To Input', Time = 0 },
+    }
+
+    local Argument_Xblock = {
+        { name = 'XBlock To 0',     Time = 0 },
+        { name = 'XBlock To 2',     Time = 2 },
+        { name = 'XBlock To 3',     Time = 3 },
+        { name = 'XBlock To 4',     Time = 4 },
+        { name = 'XBlock To Input', Time = 0 },
+    }
+
+    local Argument_Xwings = {
+        { name = 'XWings To 0',     Time = 0 },
+        { name = 'XWings To 2',     Time = 2 },
+        { name = 'XWings To 3',     Time = 3 },
+        { name = 'XWings To 4',     Time = 4 },
+        { name = 'XWings To Input', Time = 0 },
+    }
+
+    local First_Id_Lay = {}
+    local SeqNrEnd
+
+    -- variables
+    local LayX
+    local RefX
+    local LayY
+    if TLayNrRef then
+        RefX = math.floor(0 - TLay[TLayNrRef].DimensionW / 2)
+        LayY = TLay[TLayNrRef].DimensionH / 2
+    else
+        RefX = -960
+        LayY = 540
+    end
+    local LayW = 100
+    local LayH = 100
+    local LayNr = 1
+    local TCol
+    local StColCode
     local StColName
     local StringColName
-    local All_5_NrEnd
+    local SelectedGrpName = {}
+    local check = {}
+    local FirstSeqTime
+    local LastSeqTime
+    local FirstSeqGrp
+    local LastSeqGrp
+    local FirstSeqBlock
+    local LastSeqBlock
+    local FirstSeqWings
+    local LastSeqWings
+    local First_All_Color
+    local CurrentSeqNr
+    local CurrentMacroNr
+    local UsedW
+    local UsedH
+    local ColLgnCount = 0
+    local long_imgimp
+    local add_check = 0
+    local condition_string
+    local MakeX = true
+    local CallT
+    local Call_inc = 0
+    local Current_Id_Lay
 
-    -- for k in ipairs(Preset25) do
-        --     if Preset25[k].Name == 'All 5' then
-            --         Preset25[k]:Set('PresetMode', 'Universal')
-            --     end
-    -- end
+    local Fade_Element
+    local Delay_F_Element
+    local Delay_T_Element
+    local Phase_Element
+    local Group_Element
+    local Block_Element
+    local Wings_Element
 
-    -- Preset25Object[1]:Acquire() -- make recipe ?
-    -- local preset_data = GetPresetData(TCol[2])
-    -- local preset_data = GetPresetData(Preset25Object[1])
-    -- local color_R = 2.55*preset_data[3][1].absolute
-    -- Printf(color_R)
-    -- Preset25Object:Create(1) -- create preset
-    -- Preset25Object[1]:Set('Name','test') -- label preset
-    -- Preset25Object[1]:Insert(1) -- recipe 1
-    -- Preset25Object[1][1]:Set('Name','testrecipe') -- label recipe 1
-    -- Preset25Object[1][1]:Set('FadeFromX', 1) -- set 1 FadeFromX
+    local Ligne_Inc = false
 
-    -- Preset25Object[1]:Set(FadeFromX, 2) --
-    -- local ob = Preset25Object[1]
-    
+    -- fix prefix
+    local prefix_index = 1
+    local old_prefix_index
+    local prefix = 'LC' .. tostring(prefix_index) .. '_'
+    local exit = false
+    repeat
+        old_prefix_index = prefix_index
+        for k in pairs(TLay) do
+            if string.match(TLay[k].name, prefix) then
+                prefix_index = math.floor(prefix_index + 1)
+            end
+        end
+        prefix = 'LC' .. tostring(prefix_index) .. '_'
+        if old_prefix_index == prefix_index then
+            exit = true
+        end
+    until exit == true
+    -- fix name SelectedGrp
+    for g in pairs(SelectedGrp) do
+        SelectedGrpName[g] = SelectedGrp[g]:gsub(' ', '_')
+    end
 
-    -- Preset25Object:Create(1)
-    -- Preset25Object[1]:Set('Name', 'test')
-    
-    -- Preset25Object:Set('PresetMode', 'Universal')
-    -- -- CmdIndirectWait('Set Preset 25 Property PresetMode "Universal"')
-    -- CmdIndirectWait("ClearAll /nu")
-    -- CmdIndirectWait('Fixture Thru')
-    -- for col in ipairs(TCol) do
-    --     local StColName = TCol[col].name
-    --     local StringColName = string.gsub(StColName, " ", "_")
-    --     local convert = prefix .. StringColName
-    --     local Name = string.gsub(convert , " ","_")
-    --     CmdIndirectWait('At Gel ' .. SelectedGelNr .. "." .. col .. '')
-    --     CmdIndirectWait('Store DataPool ' .. pool_construct ..' Preset 25.' .. All_5_Current .. '')
-    --     CmdIndirectWait('Label DataPool ' .. pool_construct ..' Preset 25.' .. All_5_Current .. " " .. Name .. " ")
-    --     local All_5_NrEnd = All_5_Current
-    --     All_5_Current = math.floor(All_5_Current + 1)
-    -- end
+    -- fix *NrStart & use Current*Nr
+    CurrentSeqNr = SeqNrStart
+    CurrentMacroNr = MacroNrStart
 
-    -- Create Preset 25
-    All_5_NrEnd, All_5_Current = Create_Preset_25(TCol, StColName, StringColName, SelectedGelNr, prefix, All_5_NrEnd,
-        All_5_Current, pool_construct)
-    -- endCreate Preset 25
+    -- check Symbols
+    CheckSymbols(Img, ImgImp, check, add_check, long_imgimp, ImgNr)
+
+    -- Create MAtricks
+    MatrickNr = Create_Matricks(MatrickNrStart, prefix, NaLay, SelectedGrp, SelectedGrpName, MatrickNr, pool_construct)
+
+    -- Create new Layout View
+    local LayoutObject = Root().ShowData.DataPools[pool_construct].Layouts
+    LayoutObject:Create(TLayNr)
+    LayoutObject[TLayNr]:Set('Name', '' .. prefix .. NaLay)
+    -- CmdIndirectWait("Store Layout " .. TLayNr .. " \"" .. prefix .. NaLay .. "")
+    CmdIndirectWait('Select Layout ' .. TLayNr)
+
+    SelectedGelNr = tonumber(SelectedGelNr)
+    TCol = ColPath:Children()[SelectedGelNr]
+    MaxColLgn = tonumber(MaxColLgn)
+
+    -- Create Appearances Tricks Ref
+    AppNr, AppTricks = Create_Appear_Tricks(AppTricks, AppNr, prefix)
+    -- end Appearances Tricks Ref
 end
 return main
