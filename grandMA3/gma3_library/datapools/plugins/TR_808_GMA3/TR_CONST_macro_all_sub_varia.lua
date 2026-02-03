964b68d30f67ca44d2af08e6ea5e02b145964a95
richard fontaine <ririfonfon10@gmail.com>
2026-02-03 11:29:02 +0100

    Update TR_CONST_macro_all_sub_varia.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_CONST_macro_all_sub_varia.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_CONST_macro_all_sub_varia.lua
@@ -13,7 +13,7 @@ local function Check_Size_Pool(id, PoolObject)
 end
 local function main()
     local inputs = {
-        { name = "macro Number", value = "1", whiteFilter = "0123456789" },
+        { name = "macro Number", value = "1653", whiteFilter = "0123456789" },
     }
     local selectors = {
         { name = "Varia Selector", selectedValue = 1, values = { ["a"] = 1, ["b"] = 2, ["c"] = 3, ["d"] = 4, ["e"] = 5, ["f"] = 6, ["g"] = 7, ["h"] = 8 }, type = 1 },
