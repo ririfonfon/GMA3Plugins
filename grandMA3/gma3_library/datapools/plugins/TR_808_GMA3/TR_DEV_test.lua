4707a4020ced555397325e0dd4c9612f2dabf3e6
richard fontaine <ririfonfon10@gmail.com>
2026-02-12 14:30:07 +0100

    Update TR_DEV_test.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_DEV_test.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_DEV_test.lua
@@ -16,11 +16,11 @@ local function Check_Size_Pool(id, PoolObject)
 end
 local function main()
     local inputs = {
-        { name = "TAG Number", value = "214", whiteFilter = "0123456789" },
+        { name = "TAG Number", value = "230", whiteFilter = "0123456789" },
     }
-    local selectors = {
-        { name = "Varia Selector", selectedValue = 1, values = { ["a"] = 1, ["b"] = 2, ["c"] = 3, ["d"] = 4, ["e"] = 5, ["f"] = 6, ["g"] = 7, ["h"] = 8 }, type = 1 },
-    }
+    -- local selectors = {
+    --     { name = "Varia Selector", selectedValue = 1, values = { ["a"] = 1, ["b"] = 2, ["c"] = 3, ["d"] = 4, ["e"] = 5, ["f"] = 6, ["g"] = 7, ["h"] = 8 }, type = 1 },
+    -- }
 
     local TagNum, VariaSel, MacroEnd, subSel, lay_object, DataPoolNum
     local count = 1
@@ -30,8 +30,8 @@ local function main()
     local resultTable =
         MessageBox(
             {
-                title = "create macro for TR-808",
-                message = "Please enter the macro number.",
+                title = "create Tags for TR-808",
+                message = "Please enter Tags number.",
                 message_align_h = Enums.AlignmentH.Left,
                 message_align_v = Enums.AlignmentV.Top,
                 commands = { { value = 1, name = "Ok" }, { value = 0, name = "Cancel" } },
@@ -53,24 +53,32 @@ local function main()
             TagNum = tonumber(v)
         end
     end
-    for k, v in pairs(resultTable.selectors) do
-        Printf("Selector '%s' = '%d'", k, v)
-        if k == "Varia Selector" then
-            VariaSel = v
-        end
-    end
+    -- for k, v in pairs(resultTable.selectors) do
+    --     Printf("Selector '%s' = '%d'", k, v)
+    --     if k == "Varia Selector" then
+    --         VariaSel = v
+    --     end
+    -- end
     local TagObject = Root().ShowData.Tags
     local Construct_Pool = 43
     local SequenceObject = Root().ShowData.DataPools[Construct_Pool].Sequences
     local PoolObject = Root().ShowData.DataPools
 
     Check_Size_Pool(TagNum, TagObject)
-    -- TagObject:Create(TagNum)
-    local nr = TagObject:Acquire()
-    TagObject[nr.No]:Set('Name', 'test')
-    -- TagObject[TagNum]:Set('TagType', 'Kill Instant')
+    TagObject:Create(TagNum)
+    -- local nr = TagObject:Acquire()
+    -- TagObject[nr.No]:Set('Name', 'test')
+    TagObject[TagNum]:Set('Name', 'test')
+    TagObject[TagNum]:Set('TagType', 'Kill Instant')
     -- TagObject[TagNum]:Set('TagType', 'Kill Delayed')
     -- TagObject[TagNum]:Set('TagType', 'None')
+    -- TagObject[TagNum]:Insert(1)
+
+    -- TagObject[TagNum][1]:Set('DATAPOOL',PoolObject)
+    -- TagObject[TagNum][1]:Set('CLASS','Sequence')
+    -- TagObject[TagNum][1]:Set('No',1)
+    SequenceObject[1]:Set('tags',TagObject[TagNum]:Index()..':0')
+    SequenceObject[2]:Set('tags',TagObject[TagNum]:Index()..':0')
 
 end
 
