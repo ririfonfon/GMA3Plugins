fdfc5c76d32689cbc9fa519d282a3cd44ef3bf2b
richard fontaine <ririfonfon10@gmail.com>
2026-02-18 18:10:20 +0100

    Update TR_DEV_test.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_DEV_test.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_DEV_test.lua
@@ -15,21 +15,27 @@ local function Check_Size_Pool(id, PoolObject)
     end
 end
 local function main()
-    local Construct_Pool = 41
-    local Layout_Nr = 2
+    local Construct_Pool = 43
+    local Layout_Nr = 1
     local Layout_Object = Root().ShowData.DataPools[Construct_Pool].Layouts
-    local Layout_Object_C = Root().ShowData.DataPools[Construct_Pool].Layouts:Children()
+    -- local Layout_Object_C = Root().ShowData.DataPools[Construct_Pool].Layouts:Children()
     local TagObject = Root().ShowData.Tags
     local TagObject_C = Root().ShowData.Tags:Children()
     local SequenceObject = Root().ShowData.DataPools[Construct_Pool].Sequences
     local PoolObject = Root().ShowData.DataPools
+    local AppearanceObject = Root().ShowData.Appearances
 
-    for k = 1, 2408 do
-        local x = Layout_Object_C[Layout_Nr][k]:Get('posx')
-        local y = Layout_Object_C[Layout_Nr][k]:Get('posy')
-        Layout_Object_C[Layout_Nr][k]:Set('posx', x - 1030)
-        Layout_Object_C[Layout_Nr][k]:Set('posy', y + 524)
-    end
+    Check_Size_Pool(Layout_Nr,Layout_Object)
+
+    Layout_Object:Create(Layout_Nr)
+    Layout_Object[Layout_Nr]:Set('Name', 'TR-808_Build')
+
+    -- for k = 1, 2408 do
+    --     local x = Layout_Object_C[Layout_Nr][k]:Get('posx')
+    --     local y = Layout_Object_C[Layout_Nr][k]:Get('posy')
+    --     Layout_Object_C[Layout_Nr][k]:Set('posx', x - 1030)
+    --     Layout_Object_C[Layout_Nr][k]:Set('posy', y + 524)
+    -- end
 end
 
 return main
