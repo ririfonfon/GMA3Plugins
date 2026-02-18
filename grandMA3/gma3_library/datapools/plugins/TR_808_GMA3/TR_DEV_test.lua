19a6b847826ed34afc26fd575b0af7f31c8e6a80
richard fontaine <ririfonfon10@gmail.com>
2026-02-18 20:52:47 +0100

    Update TR_DEV_test.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_DEV_test.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_DEV_test.lua
@@ -23,19 +23,45 @@ local function main()
     local TagObject_C = Root().ShowData.Tags:Children()
     local SequenceObject = Root().ShowData.DataPools[Construct_Pool].Sequences
     local PoolObject = Root().ShowData.DataPools
-    local AppearanceObject = Root().ShowData.Appearances
+    local AppearanceObject = Root().ShowData.Appearances:Children()
+    local Nr
 
-    Check_Size_Pool(Layout_Nr,Layout_Object)
+    -- Check_Size_Pool(Layout_Nr, Layout_Object)
 
-    Layout_Object:Create(Layout_Nr)
-    Layout_Object[Layout_Nr]:Set('Name', 'TR-808_Build')
-
-    -- for k = 1, 2408 do
-    --     local x = Layout_Object_C[Layout_Nr][k]:Get('posx')
-    --     local y = Layout_Object_C[Layout_Nr][k]:Get('posy')
-    --     Layout_Object_C[Layout_Nr][k]:Set('posx', x - 1030)
-    --     Layout_Object_C[Layout_Nr][k]:Set('posy', y + 524)
+    -- Layout_Object:Create(Layout_Nr)
+    -- Layout_Object[Layout_Nr]:Set('Name', 'TR-808_Build')
+    -- Nr = Layout_Object[Layout_Nr]:Acquire()
+    -- Printf('Nr = ' .. Nr.No)
+    -- -- Layout_Object[Layout_Nr][Nr.No]:Set('Appearance', AppearanceObject[266])
+    -- Layout_Object[Layout_Nr][Nr.No]:Set('Appearance', '[[panelBaseGma3_png]]')
+    -- Nr = Layout_Object[Layout_Nr]:Acquire()
+    -- Printf('Nr = ' .. Nr.No)
+    -- Layout_Object[Layout_Nr][Nr.No]:Set('posx', 10)
+    -- Layout_Object[Layout_Nr][Nr.No]:Set('posy', 10)
+    -- Nr = Layout_Object[Layout_Nr]:Acquire()
+    -- Printf('Nr = ' .. Nr.No)
+    -- Layout_Object[Layout_Nr][Nr.No]:Set('width', 2000)
+    -- Layout_Object[Layout_Nr][Nr.No]:Set('height', 800)
+    -- Nr = Layout_Object[Layout_Nr]:Acquire()
+    -- Printf('Nr = ' .. Nr.No)
+    -- Layout_Object[Layout_Nr][Nr.No]:Set('visibilityelement', 'Hidden')
+    -- for i in pairs (AppearanceObject) do
+    --     Printf('AppearanceObject ' .. i .. ' = ' .. AppearanceObject[i].Name)
     -- end
+    local deb = AppearanceObject[145]
+    Printf('deb = ' .. deb)
+    Printf('deb nr = ' .. deb.No)
+    Printf('deb name = ' .. deb.Name)
+    
+
+    Nr = Layout_Object[Layout_Nr][1]:Get('Appearance')
+    if Nr ~= nil then
+        Printf('Nr = ' .. Nr)
+    else
+        Printf('Nr is nil')
+    end
+    Layout_Object[Layout_Nr][2]:Set('Appearance', Nr)
+    Layout_Object[Layout_Nr][3]:Set('Appearance', deb:AddrNative())
 end
 
 return main
