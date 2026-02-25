f3144a4c113815ad2bb4f80f58229df34a062cb1
richard fontaine <ririfonfon10@gmail.com>
2026-02-25 17:30:27 +0100

    Update TR_CONST_DEV_Layout.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_CONST_DEV_Layout.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808_GMA3/TR_CONST_DEV_Layout.lua
@@ -31,7 +31,43 @@ end
 local function main()
     local Construct_Pool = 43
     local Layout_Nr = 1
-    local object_start = { 0, 34, 17, 25, 51, 30, 1601, 1618, 82, 99, 765, 68, 88, 105, 122, 102, 116, 190, 207, 189, 206 }
+    local object_start = { 0, 34, 17, 25, 51, 30, 1601, 1618, 82, 99,
+
+        765,782,799,816,833,850,867,884,901,918,935,952, -- A 11-33
+        68,85,105,122,139,
+        102,116,190,207,189,206,
+
+        986,1003,1020,1037,1054,1071,1088,1105,1122,1139,1156,1173, -- B 34-56
+        136,153,292,309,326,
+        170,184,377,394,376,393,
+     
+        1207,1224,1241,1258,1275,1292,1309,1326,1343,1360,1377,1394, -- C 57-79
+        204,221,479,496,513,
+        238,252,564,581,563,580,
+    
+        1428,1445,1462,1479,1496,1513,1530,1547,1564,1581,1598,1615, -- D 80-102
+        272,289,666,683,700,
+        306,320,751,768,750,767,
+    
+        1649,1666,1683,1700,1717,1734,1751,1768,1785,1802,1819,1836, -- E 103-125
+        340,357,853,870,887,
+        374,388,938,955,937,954,
+
+        1870,1887,1904,1921,1938,1955,1972,1989,2006,2023,2040,2057, -- F 126-148
+        408,425,1040,1057,1074,
+        442,456,1125,1142,1124,1141,
+
+        2091,2108,2125,2142,2159,2176,2193,2210,2227,2244,2261,2278, -- G 149-171
+        476,493,1227,1244,1261,
+        510,524,1312,1329,1311,1328,
+
+        2312,2329,2346,2363,2380,2397,2414,2431,2448,2465,2482,2499, -- H 172-194
+        554,561,1414,1431,1448,
+        578,592,1499,1516,1498,1515,
+    
+        }
+
+}
     local Layout_Object = Root().ShowData.DataPools[Construct_Pool].Layouts
     -- local Layout_Object_C = Root().ShowData.DataPools[Construct_Pool].Layouts:Children()
     local TagObject = Root().ShowData.Tags
@@ -179,7 +215,7 @@ local function main()
     Layout_Object[Layout_Nr][Nr.No]:Set('Note', 'FOND')
     Set_Def(Layout_Nr, Nr, Layout_Object)
 
-    -- all_sub_#
+    -- all_sub_# & none_sub_#
     for y = 1, 2 do
         for i = 1, 12 do
             Nr = Layout_Object[Layout_Nr]:Acquire()
@@ -201,6 +237,7 @@ local function main()
         end
     end
 
+    -- mute_all_none
     Nr = Layout_Object[Layout_Nr]:Acquire()
     Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[object_start[9]])
     Layout_Object[Layout_Nr][Nr.No]:Set('posx', 459)
@@ -211,6 +248,7 @@ local function main()
     Layout_Object[Layout_Nr][Nr.No]:Set('Note', 'all_none_FOND')
     Set_Def(Layout_Nr, Nr, Layout_Object)
 
+    -- solo_all_none
     Nr = Layout_Object[Layout_Nr]:Acquire()
     Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[object_start[10]])
     Layout_Object[Layout_Nr][Nr.No]:Set('posx', 459)
@@ -230,7 +268,7 @@ local function main()
     for y = 1, 12 do
         for i = 1, 16 do
             Nr = Layout_Object[Layout_Nr]:Acquire()
-            Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[i + object_start[11]])
+            Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[i + object_start[10 + y]])
             Layout_Object[Layout_Nr][Nr.No]:Set('posx', btn_sub_x[i])
             Layout_Object[Layout_Nr][Nr.No]:Set('posy', btn_sub_y[y])
             Layout_Object[Layout_Nr][Nr.No]:Set('width', 70)
@@ -245,7 +283,7 @@ local function main()
     for y = 1, 2 do
         for i = 1, 12 do
             Nr = Layout_Object[Layout_Nr]:Acquire()
-            Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[i + object_start[12]])
+            Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[i + object_start[22 + y]])
             Layout_Object[Layout_Nr][Nr.No]:Set('posx', btn_mute_solo_all_none_x[y])
             Layout_Object[Layout_Nr][Nr.No]:Set('posy', btn_mute_solo_all_none_y[i])
             Layout_Object[Layout_Nr][Nr.No]:Set('width', 30)
@@ -260,7 +298,7 @@ local function main()
     for y = 1, 3 do
         for i = 1, 12 do
             Nr = Layout_Object[Layout_Nr]:Acquire()
-            Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[i + object_start[12 + y]])
+            Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[i + object_start[24 + y]])
             Layout_Object[Layout_Nr][Nr.No]:Set('posx', Select_Value_Matricks_x[y])
             Layout_Object[Layout_Nr][Nr.No]:Set('posy', Select_Value_Matricks_y[i])
             Layout_Object[Layout_Nr][Nr.No]:Set('width', 100)
@@ -281,7 +319,7 @@ local function main()
     --- select
     for i = 1, 12 do
         Nr = Layout_Object[Layout_Nr]:Acquire()
-        Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[i + object_start[16]])
+        Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[i + object_start[28]])
         Layout_Object[Layout_Nr][Nr.No]:Set('posx', 538)
         Layout_Object[Layout_Nr][Nr.No]:Set('posy', btn_mute_solo_all_none_y[i])
         Layout_Object[Layout_Nr][Nr.No]:Set('width', 30)
@@ -291,8 +329,9 @@ local function main()
         Set_Def(Layout_Nr, Nr, Layout_Object)
     end
 
+    --- select_all
     Nr = Layout_Object[Layout_Nr]:Acquire()
-    Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[object_start[17]])
+    Layout_Object[Layout_Nr][Nr.No]:Set('Object', SequenceObject[object_start[29]])
     Layout_Object[Layout_Nr][Nr.No]:Set('posx', 538)
     Layout_Object[Layout_Nr][Nr.No]:Set('posy', -386)
     Layout_Object[Layout_Nr][Nr.No]:Set('width', 30)
@@ -305,7 +344,7 @@ local function main()
     for y = 1, 2 do
         for i = 1, 12 do
             Nr = Layout_Object[Layout_Nr]:Acquire()
-            Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[i + object_start[17 + y]])
+            Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[i + object_start[29 + y]])
             Layout_Object[Layout_Nr][Nr.No]:Set('posx', Select_Value_Matricks_x[y + 3])
             Layout_Object[Layout_Nr][Nr.No]:Set('posy', Select_Value_Matricks_y[i])
             Layout_Object[Layout_Nr][Nr.No]:Set('width', 40)
@@ -326,7 +365,7 @@ local function main()
 
     --- Edit_Fade_TR_INPUT
     Nr = Layout_Object[Layout_Nr]:Acquire()
-    Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[object_start[20]])
+    Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[object_start[32]])
     Layout_Object[Layout_Nr][Nr.No]:Set('posx', 360)
     Layout_Object[Layout_Nr][Nr.No]:Set('posy', -386)
     Layout_Object[Layout_Nr][Nr.No]:Set('width', 40)
@@ -345,7 +384,7 @@ local function main()
 
     --- Edit_Delay_TR_INPUT
     Nr = Layout_Object[Layout_Nr]:Acquire()
-    Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[object_start[21]])
+    Layout_Object[Layout_Nr][Nr.No]:Set('Object', MacroObject[object_start[33]])
     Layout_Object[Layout_Nr][Nr.No]:Set('posx', 403)
     Layout_Object[Layout_Nr][Nr.No]:Set('posy', -386)
     Layout_Object[Layout_Nr][Nr.No]:Set('width', 40)
