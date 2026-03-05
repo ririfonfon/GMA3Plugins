e29d66672a7218b0945755f81c771d89142a6392
richard fontaine <ririfonfon10@gmail.com>
2026-03-05 12:01:42 +0100

    Update TR_Build_MAtricks.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808/TR_Build_MAtricks.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808/TR_Build_MAtricks.lua
@@ -3,6 +3,19 @@
     * 0.0.0.9
     Created by Richard Fontaine "RIRI", Mars 2026.
 --]]
+function Message_End(message)
+    local dialog = GetFocusDisplay().ScreenOverlay:Append('BaseInput')
+    dialog.H, dialog.W = 100, 400
+    local mybutton = dialog:Append('Button')
+    mybutton.Text = message
+
+    -- local AppearObject = Root().ShowData.Appearances
+    -- local myicon = dialog:Append('AppearancePreview')
+    -- myicon.Appearance = AppearObject[955]
+    -- -- myicon.Appearance = GetObject('Image 3.1')
+    -- myicon.BackColor, myicon.W = 'Global.Transparent', 60
+    -- myicon.Interactive = 'No'
+end
 
 function Check_Size_Pool(id, PoolObject)
     if not id then
@@ -20,8 +33,8 @@ function Check_Size_Pool(id, PoolObject)
         PoolObject:Resize(newsize)
     end
 end
-function Build_MAtricks()
 
+function Build_MAtricks()
     local MAtricksNum = 1
     local Construct_Pool = 43
     local MAtricksObject = Root().ShowData.DataPools[Construct_Pool].MAtricks
@@ -30,4 +43,30 @@ function Build_MAtricks()
     Check_Size_Pool(i, MAtricksObject)
     MAtricksObject:Create(i)
     MAtricksObject[i]:Set('Name', 'TR_INPUT')
+end
+
+function Check_DataPool(Construct_Pool)
+    local PoolObject = Root().ShowData.DataPools
+    if PoolObject[Construct_Pool] == nil then
+        PoolObject:Create(Construct_Pool)
+        Printf('Create')
+        coroutine.yield(0.1)
+    end
+
+    local SequenceObject = Root().ShowData.DataPools[Construct_Pool].Sequences:Children() or nil
+    local MacroObject = Root().ShowData.DataPools[Construct_Pool].Macros:Children() or nil
+    local Size_Seq, Size_Macro = 0, 0
+
+    if SequenceObject ~= nil then
+        for S in pairs(SequenceObject) do
+            Size_Seq = S
+        end
+    end
+    if MacroObject ~= nil then
+        for S in pairs(MacroObject) do
+            Size_Macro = S
+        end
+    end
+
+    Printf('seq ' .. Size_Seq .. ' macro ' .. Size_Macro)
 end
