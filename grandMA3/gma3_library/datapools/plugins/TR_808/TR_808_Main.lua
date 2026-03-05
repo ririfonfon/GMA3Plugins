3d2797e053ad995440027ef9f149db336711a405
richard fontaine <ririfonfon10@gmail.com>
2026-03-05 12:01:36 +0100

    Update TR_808_Main.lua

--- grandMA3/gma3_library/datapools/plugins/TR_808/TR_808_Main.lua
+++ grandMA3/gma3_library/datapools/plugins/TR_808/TR_808_Main.lua
@@ -29,7 +29,50 @@ local function main(displayHandle)
     end
 
     Cmd('Set UserProfile *.15 Property "keyboardshortcutsactive" false')
-    
+    local Construct_Pool, Name_Construct_Pool, Name_Layout
+    local Call_Pool = myHandle:FindParent(DataPool():GetClass())
 
+    local inputs = {
+        { name = "Free DataPool for TR_808", value = "46",       whiteFilter = "0123456789" },
+        { name = "Name of DataPool",         value = "TR_808", maxTextLength = 20 },
+        { name = "Name of Layout",          value = "TR_808", maxTextLength = 20 },
+    }
+
+
+
+    -- open messagebox:
+    local resultTable =
+        MessageBox(
+            {
+                title = "TR_808 on GMA3",
+                message = "Please enter number to set.",
+                message_align_h = Enums.AlignmentH.Left,
+                message_align_v = Enums.AlignmentV.Top,
+                commands = { { value = 1, name = "Ok" }, { value = 0, name = "Cancel" } },
+                inputs = inputs,
+                backColor = "Global.Default",
+                icon = "logo_small",
+                titleTextColor = "Global.AlertText",
+                messageTextColor = "Global.Text",
+                autoCloseOnInput = true
+            }
+        )
+
+    -- print results:
+
+    for k, v in pairs(resultTable.inputs) do
+        if k == 'Free DataPool for TR_808' then
+            Construct_Pool = tonumber(v)
+        elseif k == 'Name of DataPool' then
+            Name_Construct_Pool = (v)
+        elseif k == 'Name of Layout' then
+            Name_Layout = (v)
+        end
+    end
+   
+
+    Check_DataPool(Construct_Pool)
+
+    Printf('REturn')
 end
 return main
