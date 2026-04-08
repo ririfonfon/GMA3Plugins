916c361a42e4ed16211dca808dd37e4f68802af3
richard fontaine <ririfonfon10@gmail.com>
2026-04-08 18:54:49 +0200

    Update Sound_Build_Layout.lua

--- grandMA3/gma3_library/datapools/plugins/SOUND/Sound_Build_Layout.lua
+++ grandMA3/gma3_library/datapools/plugins/SOUND/Sound_Build_Layout.lua
@@ -44,39 +44,39 @@ function Set_Def(L_N, N, Obj)
 end
 
 function Build_Layout(Construct_Pool, Name_Layout)
-    local Layout_Nr  = 1
-    local Sound_Type = { 'All', 'Bass', 'Mid', 'High', 'Band1', 'Band2', 'Band3', 'Band4', 'Band5', 'Band6', 'Band7' }
-    local Layout_Object = Root().ShowData.DataPools[Construct_Pool].Layouts
+    local Layout_Nr      = 1
+    local Sound_Type     = { 'All', 'Bass', 'Mid', 'High', 'Band1', 'Band2', 'Band3', 'Band4', 'Band5', 'Band6', 'Band7' }
+    local Layout_Object  = Root().ShowData.DataPools[Construct_Pool].Layouts
     local SequenceObject = Root().ShowData.DataPools[Construct_Pool].Sequences
-    local MacroObject = Root().ShowData.DataPools[Construct_Pool].Macros
+    local MacroObject    = Root().ShowData.DataPools[Construct_Pool].Macros
     local Nr
 
-    local titre_x = {
+    local titre_x        = {
         -400, -400, -400, -350, 200, 750,
         -400, -400, -400, -350, 200, 750,
         -400, -400, -400, -350, 200, 750,
         -400, -400, -400, -350, 200
     }
-    local titre_y = {
+    local titre_y        = {
         100, 50, 0, 150, 150, 150,
         -100, -150, -200, -50, -50, -50,
         -300, -350, -400, -250, -250, -250,
         -500, -550, -600, -450, -450
     }
-    local titre_w = {
+    local titre_w        = {
         50, 50, 50, 500, 500, 500,
         50, 50, 50, 500, 500, 500,
         50, 50, 50, 500, 500, 500,
         50, 50, 50, 500, 500
     }
-    local texte = {
+    local texte          = {
         'P1', 'P2', 'P3', 'Sound All', 'Sound Bass', 'Sound Mid',
         'P1', 'P2', 'P3', 'Sound High', 'Sound Band 1', 'Sound Band 2',
         'P1', 'P2', 'P3', 'Sound Band 3', 'Sound Band 4', 'Sound Band 5',
         'P1', 'P2', 'P3', 'Sound Band 6', 'Sound Band 7'
     }
 
-    local macro_x = {
+    local macro_x        = {
         -350, -250, -150, -50, 50,
         -350, -250, -150, -50, 50,
         -350, -250, -150, -50, 50,
@@ -111,7 +111,7 @@ function Build_Layout(Construct_Pool, Name_Layout)
         200, 300, 400, 500, 600,
         200, 300, 400, 500, 600,
     }
-    local macro_y = {
+    local macro_y        = {
         100, 100, 100, 100, 100,
         50, 50, 50, 50, 50,
         0, 0, 0, 0, 0,
@@ -147,8 +147,8 @@ function Build_Layout(Construct_Pool, Name_Layout)
         -600, -600, -600, -600, -600,
     }
 
-    local S_V_M_Color = { '8080FFFF', '80F80FFF', 'FF8080FF', '80FFFFFF', 'FFFF80FF' }
-    local S_V_M_Text = { 'Group', 'Value', 'Matricks', 'None/None', 'None/None' }
+    local S_V_M_Color    = { '8080FFFF', 'FF0080FF', 'FFFF80FF', '00FF00FF', 'FF8000FF' }
+    local S_V_M_Text     = { 'Group', 'Value', 'Matricks', 'None/None', 'None/None' }
 
     Check_Size_Pool(Layout_Nr, Layout_Object)
     Layout_Object:Create(Layout_Nr)
@@ -166,7 +166,7 @@ function Build_Layout(Construct_Pool, Name_Layout)
     Layout_Object[Layout_Nr][Nr.No]:Set('width', 50)
     Layout_Object[Layout_Nr][Nr.No]:Set('height', 50)
     Layout_Object[Layout_Nr][Nr.No]:Set('action', 'Go+')
-    Layout_Object[Layout_Nr][Nr.No]:Set('Note', 'FOND')
+    Layout_Object[Layout_Nr][Nr.No]:Set('Note', 'On&Off')
     Set_Def(Layout_Nr, Nr, Layout_Object)
 
     -- titre
@@ -176,7 +176,7 @@ function Build_Layout(Construct_Pool, Name_Layout)
         Layout_Object[Layout_Nr][Nr.No]:Set('posy', titre_y[i])
         Layout_Object[Layout_Nr][Nr.No]:Set('width', titre_w[i])
         Layout_Object[Layout_Nr][Nr.No]:Set('height', 50)
-        Layout_Object[Layout_Nr][Nr.No]:Set('Note', 'FOND')
+        Layout_Object[Layout_Nr][Nr.No]:Set('Note', 'Titre')
         Layout_Object[Layout_Nr][Nr.No]:Set('customtexttext', texte[i])
         Layout_Object[Layout_Nr][Nr.No]:Set('Name', texte[i])
         Layout_Object[Layout_Nr][Nr.No]:Set('visibilityborder', 'Visible')
