acc6d970d7dd069e20a7c29baf117fbc48c93f59
richard fontaine <ririfonfon10@gmail.com>
2026-04-21 15:08:50 +0200

    Update Sound_Construct.lua

--- grandMA3/gma3_library/datapools/plugins/SOUND/Sound_Construct.lua
+++ grandMA3/gma3_library/datapools/plugins/SOUND/Sound_Construct.lua
@@ -4,9 +4,16 @@
     Created by Richard Fontaine "RIRI", April 2026.
 --]]
 
+function Sound_Dialog_End(message)
+    local dialog = GetFocusDisplay().ScreenOverlay:Append('BaseInput')
+    dialog.H, dialog.W = 800, 800
+    local mybutton = dialog:Append('Button')
+    mybutton.Text = message
+end
+
 function Sound_Construct(Construct_Pool, Grp_Start, All_4_NrStart, Univers, Address, Fid, SeqNrStart, MacroNrStart,
                          TLayNr, NaLay)
-    Dialog_End('Sound By Riri Build')
+    Sound_Dialog_End('Sound By Riri Build')
     Cmd('ClearAll')
     Cmd('Store DataPool ' .. Construct_Pool .. ' Group ' .. Grp_Start .. ' /Overwrite /NoConfirmation')
     Cmd('Store DataPool ' .. Construct_Pool .. ' Preset 24.' .. All_4_NrStart .. ' /Overwrite /NoConfirmation')
