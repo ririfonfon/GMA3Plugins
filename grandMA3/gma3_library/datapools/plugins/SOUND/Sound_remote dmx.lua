92c85571f86397f1c42f378686004ca91b94f90c
richard fontaine <ririfonfon10@gmail.com>
2026-04-24 14:57:04 +0200

    Update Sound_remote dmx.lua

--- grandMA3/gma3_library/datapools/plugins/SOUND/Sound_remote dmx.lua
+++ grandMA3/gma3_library/datapools/plugins/SOUND/Sound_remote dmx.lua
@@ -4,19 +4,23 @@
     Created by Richard Fontaine "RIRI", Mars 2026.
 --]]
 
-function SOUND_Remote_Dmx(Construct_Pool, SeqNum, Univers, Address)
+function SOUND_Remote_Dmx(Construct_Pool, SeqNum, Univers, Address, prefix)
     -- local Construct_Pool = 5
     local SequenceObject = Root().ShowData.DataPools[Construct_Pool].Sequences
     local RemoteObject   = Root().ShowData.Remotes.DmxRemotes
     local startSeq       = SeqNum + 12
     local Count          = 1
-    local endSeq         = startSeq + 10
     local Sound_Type     = { 'All', 'Bass', 'Mid', 'High', 'Band1', 'Band2', 'Band3', 'Band4', 'Band5', 'Band6', 'Band7' }
 
+    if prefix == 'B' then
+        startSeq = startSeq + 123
+        Address = Address + 11
+    end
+    local endSeq = startSeq + 10
 
     for i = startSeq, endSeq, 1 do
         local a = RemoteObject:Acquire()
-        RemoteObject[a.No]:Set('Name', 'Sound ' .. Sound_Type[Count])
+        RemoteObject[a.No]:Set('Name', prefix .. 'Sound ' .. Sound_Type[Count])
         RemoteObject[a.No]:Set('Address', "" .. Univers .. ".0" .. Address - 1 + Count .. "")
         RemoteObject[a.No]:Set('Target', SequenceObject[i])
         RemoteObject[a.No]:Set('Fader', 'Temp')
