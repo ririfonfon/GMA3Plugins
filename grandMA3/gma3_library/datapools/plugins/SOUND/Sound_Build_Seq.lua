95731ee1b3d46b8188dba94424f491e125708c38
richard fontaine <ririfonfon10@gmail.com>
2026-04-24 14:56:53 +0200

    Update Sound_Build_Seq.lua

--- grandMA3/gma3_library/datapools/plugins/SOUND/Sound_Build_Seq.lua
+++ grandMA3/gma3_library/datapools/plugins/SOUND/Sound_Build_Seq.lua
@@ -44,18 +44,23 @@ function SOUND_Sequence_Defo(SequenceObject, i)
     SequenceObject[i]:Set('TIMINGGOBACKFAST', 'Default')
 end
 
-function SOUND_Build_Seq(Construct_Pool, SeqNum, All_4_NrStart, Grp_Start, Fid)
+function SOUND_Build_Seq(Construct_Pool, SeqNum, All_4_NrStart, Grp_Start, Fid, prefix)
     local Sound_Type     = { 'All', 'Bass', 'Mid', 'High', 'Band1', 'Band2', 'Band3', 'Band4', 'Band5', 'Band6', 'Band7' }
-    local SeqEnd         = SeqNum + 10
     local TypeSel        = 1
     local SequenceObject = Root().ShowData.DataPools[Construct_Pool].Sequences
     local Preset4Object  = Root().ShowData.DataPools[Construct_Pool].PresetPools[24]
     local GroupObject    = Root().ShowData.DataPools[Construct_Pool].Groups
-
-    local inc            = 0
+    if prefix == 'B' then
+        Grp_Start = Grp_Start + 13
+        Fid = Fid + 11
+        All_4_NrStart = All_4_NrStart + 12
+        SeqNum = SeqNum + 123
+    end
+    local SeqEnd         = SeqNum + 10
+    local inc = 0
     for i = Grp_Start, Grp_Start + 10 do
         GroupObject:Create(i)
-        GroupObject[i]:Set('Name', 'Sound ' .. Sound_Type[TypeSel])
+        GroupObject[i]:Set('Name', prefix .. 'Sound ' .. Sound_Type[TypeSel])
         Cmd("AutoCreate Fixture " ..
             Fid + inc .. " At DataPool " .. Construct_Pool .. " Group " .. i .. " /All /NoConfirmation ")
         GroupObject[i]:Set('Mode', 'Positive')
@@ -64,15 +69,15 @@ function SOUND_Build_Seq(Construct_Pool, SeqNum, All_4_NrStart, Grp_Start, Fid)
     end
     GroupObject:Create(Grp_Start + 11)
     Cmd("AutoCreate Fixture " ..
-    Fid ..
-    "Thru" .. Fid + 10 .. "At DataPool " .. Construct_Pool .. " Group " .. Grp_Start + 11 .. " /All /NoConfirmation ")
+        Fid ..
+        "Thru" .. Fid + 10 .. "At DataPool " .. Construct_Pool .. " Group " .. Grp_Start + 11 .. " /All /NoConfirmation ")
     GroupObject[Grp_Start + 11]:Set('Mode', 'Negative')
-    GroupObject[Grp_Start + 11]:Set('Name', 'MASTER ALL SOUND')
+    GroupObject[Grp_Start + 11]:Set('Name', prefix .. 'MASTER ALL SOUND')
     TypeSel = 1
 
     for i = All_4_NrStart, All_4_NrStart + 10 do
         Preset4Object:Create(i)
-        Preset4Object[i]:Set('Name', 'Sound ' .. Sound_Type[TypeSel])
+        Preset4Object[i]:Set('Name', prefix .. 'Sound ' .. Sound_Type[TypeSel])
         Cmd("SelectFixtures DataPool " .. Construct_Pool .. " Group " .. GroupObject[i].No)
         Cmd(" Attribute 'Dimmer' At SoundChannel '" .. Sound_Type[TypeSel] .. "'")
         Cmd("Store " .. Preset4Object[i] .. " /Merge")
@@ -109,7 +114,7 @@ function SOUND_Build_Seq(Construct_Pool, SeqNum, All_4_NrStart, Grp_Start, Fid)
     SOUND_Check_Size_Pool(SeqEnd + 1, SequenceObject)
     SequenceObject:Create(SeqEnd + 1)
     local Seq_On_Off = SeqEnd + 1
-    SequenceObject[SeqEnd + 1]:Set('Name', 'On_Off Sound ')
+    SequenceObject[SeqEnd + 1]:Set('Name', prefix .. 'On_Off Sound ')
     SequenceObject[SeqEnd + 1]:Set('Appearance', '[[Switch_Off_png]]')
     SOUND_Sequence_Defo(SequenceObject, SeqEnd + 1)
     SequenceObject[SeqEnd + 1]:Set('AUTOSTART', 'No')
@@ -136,7 +141,7 @@ function SOUND_Build_Seq(Construct_Pool, SeqNum, All_4_NrStart, Grp_Start, Fid)
     for i = SeqNum, SeqEnd, 1 do
         SOUND_Check_Size_Pool(i, SequenceObject)
         SequenceObject:Create(i)
-        SequenceObject[i]:Set('Name', 'RecepieSound ' .. Sound_Type[TypeSel])
+        SequenceObject[i]:Set('Name', prefix .. 'RecepieSound ' .. Sound_Type[TypeSel])
         SOUND_Sequence_Defo(SequenceObject, i)
         SequenceObject[i]:Set('AUTOSTART', 'No')
         SequenceObject[i]:Set('AUTOSTOP', 'No')
@@ -167,7 +172,7 @@ function SOUND_Build_Seq(Construct_Pool, SeqNum, All_4_NrStart, Grp_Start, Fid)
         for i = SeqNum, SeqEnd, 1 do
             SOUND_Check_Size_Pool(i, SequenceObject)
             SequenceObject:Create(i)
-            SequenceObject[i]:Set('Name', "MEM_" .. k .. "_RecepieSound " .. Sound_Type[TypeSel])
+            SequenceObject[i]:Set('Name', prefix .. "MEM_" .. k .. "_RecepieSound " .. Sound_Type[TypeSel])
             SOUND_Sequence_Defo(SequenceObject, i)
             SequenceObject[i]:Set('AUTOSTART', 'No')
             SequenceObject[i]:Set('AUTOSTOP', 'No')
