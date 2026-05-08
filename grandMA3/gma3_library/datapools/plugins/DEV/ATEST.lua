33d45ce8dc2ba3d4185f0d565edf4576a9b55179
richard fontaine <ririfonfon10@gmail.com>
2026-05-08 14:25:06 +0200

    Create ATEST.lua

---
+++ grandMA3/gma3_library/datapools/plugins/DEV/ATEST.lua
@@ -0,0 +1,5 @@
+return function()
+    local FixtureGroups = Root().ShowData.DataPools[5].Groups[25]
+    -- FixtureGroups:Set('Master', 100)
+    Sound_Dialog_End('ALL Good Sound By Riri Finish \n\n\n Remenber\n\n AMasterALLSOUND \n & \n BMasterALLSOUND \n\n Value are 0 \n\n Make Cross remote \n\n or set a value\n\n\n\n Enjoy')
+end
