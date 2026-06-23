938441aa6b28ed6899eeabb72be89b6fc8622cf0
richard fontaine <ririfonfon10@gmail.com>
2026-06-23 16:59:14 +0200

    check size doublon

--- grandMA3/gma3_library/datapools/plugins/LayoutColor/LC_Cmd.lua
+++ grandMA3/gma3_library/datapools/plugins/LayoutColor/LC_Cmd.lua
@@ -222,32 +222,6 @@ function LC_Set_Def(L_N, N, Obj)
     Obj[L_N][N.No]:Set('fullresolution', 'Yes')
 end
 
-function LC_Check_Size_Pool(id, PoolObject)
-    if not id then
-        Printf('Acquire')
-        return PoolObject:Acquire()
-    end
-    local idtype = math.type(id) or type(id)
-    if idtype ~= 'integer' then
-        LC_Dialog_End('Error : wrong argument expected integer got ' .. idtype)
-        error('wrong argument expected integer got ' .. idtype)
-    end
-    if IsObjectValid(PoolObject[id]) then
-        LC_Dialog_End('Error : id is already used : ' .. id)
-        error('id is already used : ' .. id)
-    end
-    local maxsize = PoolObject:MaxCount()
-    if id < 1 or id > maxsize then
-        LC_Dialog_End('Error : id out of range')
-        error('id out of range')
-    end
-    local poolsize = PoolObject:Count()
-    if id > poolsize then
-        local newsize = math.min(maxsize, math.ceil(id / 1000) * 1000)
-        PoolObject:Resize(newsize)
-    end
-end
-
 function LC_Sequence_Defo(SequenceObject, i)
     SequenceObject[i]:Set('PREFERCUEAPPEARANCE', 'Yes')
     SequenceObject[i]:Set('AUTOSTART', 'Yes')
