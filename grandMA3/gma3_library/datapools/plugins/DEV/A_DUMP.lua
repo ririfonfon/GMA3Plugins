4141e700161b230fba18efaece7ed9cc49f944c0
richard fontaine <ririfonfon10@gmail.com>
2026-08-03 12:03:31 +0200

    Update A_DUMP.lua

--- grandMA3/gma3_library/datapools/plugins/DEV/A_DUMP.lua
+++ grandMA3/gma3_library/datapools/plugins/DEV/A_DUMP.lua
@@ -1,7 +1,9 @@
 return function()
     -- local Dump_Object = Root().ShowData.ShowSettings.MidiSettings:Children()
     -- local Dump_Object = Root().ShowData.ShowSettings.MidiSettings:Children()
-    local Dump_Object = Root().StationSettings:Children()
+    -- local Dump_Object = Root().StationSettings:Children()
+    local Dump_Object = ShowData().OSCBase:Children()
+    
     for i, content in pairs(Dump_Object) do
         Echo(i)
     end
